e100ec6ae8e81daa2916ad3c6e8df7754764fb78
Erjan Gavalji <erjan.gavalji@gmail.com>
2015-11-11 15:18:00 +0200

    Initial commit

---
+++ packages/template-hello-world-ts/app.ts
@@ -0,0 +1,7 @@
+﻿import application = require("application");
+application.mainModule = "main-page";
+
+// Remove this in the AppBuilder templates
+application.cssFile = "./app.css"
+
+application.start();
