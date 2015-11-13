7f86bec2d956510c816fbcf5af17c8a23d440566
Vasil Chimev <Vasil.Chimev@telerik.com>
2015-11-13 13:54:57 +0200

    Merge pull request #31 from NativeScript/master
    Merge master to release.

--- packages/template-hello-world/app.js
+++ packages/template-hello-world/app.js
@@ -1,3 +1,4 @@
+/// <reference path="../node_modules/tns-core-modules/tns-core-modules.d.ts" /> Enable smart suggestions and completions in Visual Studio Code JavaScript projects.
 var application = require("application");
 application.mainModule = "main-page";
 application.cssFile = "./app.css";
