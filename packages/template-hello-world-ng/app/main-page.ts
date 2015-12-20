17f8e75e7de5c785945201222434ce540ff5dbe6
Hristo Deshev <hristo@deshev.com>
2015-12-20 14:56:33 +0200

    Update for Angular 2 beta 0

--- packages/template-hello-world-ng/app/main-page.ts
+++ packages/template-hello-world-ng/app/main-page.ts
@@ -2,14 +2,14 @@ import 'reflect-metadata';
 import {TextView} from 'ui/text-view';
 import {topmost} from 'ui/frame';
 import {nativeScriptBootstrap} from 'nativescript-angular/application';
-import {Component} from 'angular2/angular2';
+import {Component} from 'angular2/core';
 
 
 @Component({
 	selector: 'main',
 	template: `
 <StackLayout orientation='vertical'>
-    <Label [text]='message' class='title'></Label>
+    <Label [text]='message' class='title' (tap)='message = "OHAI"'></Label>
 </StackLayout>
 `,
 })
