be304f325d456c83d34aa2a7ba8f65b914ba5739
Hristo Deshev <hristo+lo@deshev.com>
2016-02-03 11:27:30 +0200

    Merge pull request #3 from NativeScript/angular-consistency
    Adjusting naming/coding conventions to match Angular 2’s own guide as much as possible

--- packages/template-hello-world-ng/app/app.ts
+++
@@ -1,6 +0,0 @@
-// this import should be first in order to load some required settings (like globals and reflect-metadata)
-import {nativeScriptBootstrap} from 'nativescript-angular/application';
-
-import {MainPage} from './main-page';
-
-nativeScriptBootstrap(MainPage);
