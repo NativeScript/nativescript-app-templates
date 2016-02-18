6058c4943643bb00851e013f923457577ae46eb9
Hristo Deshev <hristo@deshev.com>
2016-02-18 13:28:14 +0200

    Turn into a real nativescript CLI template.

---
+++ packages/template-hello-world-ng/main.js
@@ -0,0 +1,5 @@
+// this import should be first in order to load some required settings (like globals and reflect-metadata)
+var application_1 = require("nativescript-angular/application");
+var app_component_1 = require("./app.component");
+application_1.nativeScriptBootstrap(app_component_1.AppComponent);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIm1haW4udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsMEdBQTBHO0FBQzFHLDRCQUFvQyxrQ0FBa0MsQ0FBQyxDQUFBO0FBQ3ZFLDhCQUEyQixpQkFBaUIsQ0FBQyxDQUFBO0FBRTdDLG1DQUFxQixDQUFDLDRCQUFZLENBQUMsQ0FBQyJ9
