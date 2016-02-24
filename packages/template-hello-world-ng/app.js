ee8df92b1cbe1e2654f6d482cb650b9f79b8936f
Hristo Deshev <hristo@deshev.com>
2016-02-24 16:30:15 +0200

    Delete *.js files

--- packages/template-hello-world-ng/app.js
+++
@@ -1,5 +0,0 @@
-// this import should be first in order to load some required settings (like globals and reflect-metadata)
-var application_1 = require('nativescript-angular/application');
-var main_page_1 = require('./main-page');
-application_1.nativeScriptBootstrap(main_page_1.MainPage);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiYXBwLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBLDBHQUEwRztBQUMxRyw0QkFBb0Msa0NBQWtDLENBQUMsQ0FBQTtBQUV2RSwwQkFBdUIsYUFBYSxDQUFDLENBQUE7QUFFckMsbUNBQXFCLENBQUMsb0JBQVEsQ0FBQyxDQUFDIn0=
