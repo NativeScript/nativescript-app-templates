b7b359566c930e23b20f9e315d28e1fa1517a9ac
vakrilov <alexander.vakrilov@gmail.com>
2016-04-26 14:02:06 +0300

    Template updated

--- packages/template-hello-world-ng/app.component.ts
+++ packages/template-hello-world-ng/app.component.ts
@@ -3,11 +3,27 @@ import {Component} from "angular2/core";
 @Component({
     selector: "my-app",
     template: `
-<StackLayout orientation="vertical">
-    <Label [text]="message" class="title" (tap)="message = 'OHAI'"></Label>
+<StackLayout>
+    <Label text="Tap the button" class="title"></Label>
+    
+    <Button text="TAP" (tap)="onTap()"></Button>
+
+    <Label [text]="message" class="message" textWrap="true"></Label>
 </StackLayout>
 `,
 })
 export class AppComponent {
-    public message: string = "Hello, Angular!";
+    public counter: number = 16;
+
+    public get message(): string {
+        if (this.counter > 0) {
+            return this.counter + " taps left";
+        } else {
+            return "Hoorraaay! \nYou are ready to start building!";
+        }
+    }
+    
+    public onTap() {
+        this.counter--;
+    }
 }
