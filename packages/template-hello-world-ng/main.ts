64f469231237aef45881e27060183b0f7af3046b
Vasil Chimev <Vasil.Chimev@telerik.com>
2016-08-31 11:13:37 +0300

    Merge pull request #23 from NativeScript/nnikolov/TemplateFixForRc5
    Fix template for nativescript-angular 0.4.0

--- packages/template-hello-world-ng/main.ts
+++ packages/template-hello-world-ng/main.ts
@@ -1,5 +1,13 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
-import {nativeScriptBootstrap} from "nativescript-angular/application";
-import {AppComponent} from "./app.component";
+import { platformNativeScriptDynamic, NativeScriptModule } from "nativescript-angular/platform";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
 
-nativeScriptBootstrap(AppComponent);
+@NgModule({
+    declarations: [AppComponent],
+    bootstrap: [AppComponent],
+    imports: [NativeScriptModule],
+})
+class AppComponentModule {}
+
+platformNativeScriptDynamic().bootstrapModule(AppComponentModule);
