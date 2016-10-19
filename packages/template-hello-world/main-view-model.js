f45012ebf75c08e31cef6bc8f74245fe29a9c9f4
Nathan Walker <walkerrunpdx@gmail.com>
2016-10-19 13:20:50 -0700

    feat: template setup

--- packages/template-hello-world/main-view-model.js
+++ packages/template-hello-world/main-view-model.js
@@ -1,24 +1,8 @@
 var Observable = require("data/observable").Observable;
-var utilityModule = require("utils/utils");
-
-function getMessage(counter) {
-  if (counter <= 0) {
-    return "Hoorraaay! You unlocked the NativeScript clicker achievement!";
-  } else {
-    return counter + " taps left";
-  }
-}
 
 function createViewModel(page) {
   var page = page;
   var viewModel = new Observable();
-  viewModel.counter = 42;
-  viewModel.message = getMessage(viewModel.counter);
-
-  viewModel.onTap = function () {
-    this.counter--;
-    this.set("message", getMessage(this.counter));
-  };
 
   viewModel.viewTheme = function () {
     page.showModal("modal", {
