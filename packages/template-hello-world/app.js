26dc2a5901cef013e97aa250a348ff80ef81328e
Hristo Deshev <hristo+lo@deshev.com>
2016-10-27 15:56:49 +0300

    Merge pull request #54 from NativeScript/hdeshev/bundle-config
    Include sample bundle configuration.

--- packages/template-hello-world/app.js
+++ packages/template-hello-world/app.js
@@ -1,2 +1,4 @@
+require("./bundle-config");
 var application = require("application");
+
 application.start({ moduleName: "main-page" });
---
+++ packages/template-hello-world/bundle-config.js
@@ -0,0 +1,8 @@
+if (global.TNS_WEBPACK) {
+    //registers tns-core-modules UI framework modules
+    require("bundle-entry-points");
+
+    //register application modules
+    global.registerModule("main-page", function () { return require("./main-page"); });
+    global.registerModule("modal", function () { return require("./modal"); });
+}
