dc7ed42360f35907b2e958c0447680078e8a3e18
Hristo Deshev <hristo+lo@deshev.com>
2016-10-28 11:37:59 +0300

    Merge pull request #19 from NativeScript/vlaeva/refactoring
    Refactoring: css, indentation

--- packages/template-hello-world-ts/main-page.ts
+++ packages/template-hello-world-ts/main-page.ts
@@ -4,7 +4,7 @@ import { HelloWorldModel } from './main-view-model';
 
 // Event handler for Page "navigatingTo" event attached in main-page.xml
 export function navigatingTo(args: EventData) {
-  // Get the event sender
-  let page = <Page>args.object;
-  page.bindingContext = new HelloWorldModel();
+    // Get the event sender
+    let page = <Page>args.object;
+    page.bindingContext = new HelloWorldModel();
 }
--- packages/template-hello-world-ts/main-view-model.ts
+++ packages/template-hello-world-ts/main-view-model.ts
@@ -2,38 +2,38 @@ import {Observable} from 'data/observable';
 
 export class HelloWorldModel extends Observable {
 
-  private _counter: number;
-  private _message: string;
+    private _counter: number;
+    private _message: string;
 
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    // Initialize default values.
-    this._counter = 42;
-    this.updateMessage();
-  }
-
-  get message(): string {
-    return this._message;
-  }
-  
-  set message(value: string) {
-    if (this._message !== value) {
-      this._message = value;
-      this.notifyPropertyChange('message', value)
+        // Initialize default values.
+        this._counter = 42;
+        this.updateMessage();
     }
-  }
 
-  public onTap() {
-    this._counter--;
-    this.updateMessage();
-  }
-
-  private updateMessage() {
-    if (this._counter <= 0) {
-      this.message = 'Hoorraaay! You unlocked the NativeScript clicker achievement!';
-    } else {
-      this.message = `${this._counter} taps left`;
+    get message(): string {
+        return this._message;
     }
-  }
+    
+    set message(value: string) {
+        if (this._message !== value) {
+            this._message = value;
+            this.notifyPropertyChange('message', value)
+        }
+    }
+
+    public onTap() {
+        this._counter--;
+        this.updateMessage();
+    }
+
+    private updateMessage() {
+        if (this._counter <= 0) {
+            this.message = 'Hoorraaay! You unlocked the NativeScript clicker achievement!';
+        } else {
+            this.message = `${this._counter} taps left`;
+        }
+    }
 }
