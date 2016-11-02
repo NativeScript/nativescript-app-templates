43933ca4bb320250d5c435a00b1142e4edd8e71a
Dimitar Topuzov <dimitar.topuzov@telerik.com>
2016-11-02 16:09:00 +0200

    Add comments

--- packages/template-hello-world-ts/app.ts
+++ packages/template-hello-world-ts/app.ts
@@ -1,4 +1,15 @@
-﻿import "./bundle-config";
+﻿/*
+In NativeScript, the app.ts file is the entry point to your application.
+You can use this file to perform app-level initialization, but the primary
+purpose of the file is to pass control to the app’s first module.
+*/
+
+import "./bundle-config";
 import * as app from 'application';
 
 app.start({ moduleName: 'main-page' });
+
+/*
+Do not place any code after the application has been started as it will not
+be executed on iOS.
+*/
