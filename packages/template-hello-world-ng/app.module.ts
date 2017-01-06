0c2e13a1a8cf3b97408f3fdd9c92f90a48cd2b2b
Hristo Deshev <hristo@deshev.com>
2017-01-06 15:27:42 +0200

    fix(AoT): prevent @angular/compiler from bundling.

--- packages/template-hello-world-ng/app.module.ts
+++ packages/template-hello-world-ng/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "nativescript-angular/platform";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { AppRoutingModule } from "./app.routing";
 import { AppComponent } from "./app.component";
 
