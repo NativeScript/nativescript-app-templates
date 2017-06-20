7025e7d2bfbbd79e24afe52463e80314d1931cff
Alexander Djenkov <djenkov@progress.com>
2017-06-20 15:40:41 +0300

    Fix tslint problems

--- packages/template-blank-ng/app.module.ts
+++ packages/template-blank-ng/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule, NgModuleFactoryLoader, NO_ERRORS_SCHEMA } from "@angular/core
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NSModuleFactoryLoader } from "nativescript-angular/router";
 
-import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
     bootstrap: [
--- packages/template-blank-ng/home/home.module.ts
+++ packages/template-blank-ng/home/home.module.ts
@@ -1,8 +1,8 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 
-import { HomeComponent } from "./home.component";
 import { HomeRoutingModule } from "./home-routing.module";
+import { HomeComponent } from "./home.component";
 
 @NgModule({
     imports: [
