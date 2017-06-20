8d7c1b295f58362426671c0260a5eeccfd3bcc28
Alexander Djenkov <a.djenkov90@gmail.com>
2017-06-20 15:54:28 +0300

    Merge pull request #1 from NativeScript/djenkov/init-template
    Create Blank template for Vanilla Typescript

---
+++ packages/template-blank-ts/bundle-config.ts
@@ -0,0 +1,7 @@
+if ((<any>global).TNS_WEBPACK) {
+    //registers tns-core-modules UI framework modules
+    require("bundle-entry-points");
+
+    //register application modules
+    global.registerModule("home/home-page", () => require("./home/home-page"));
+}
