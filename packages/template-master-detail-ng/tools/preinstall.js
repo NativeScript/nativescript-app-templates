852c8ac03db05e404a198543530d6ad81b0192f3
Manol Donev <manoldonev@users.noreply.github.com>
2017-06-30 18:00:26 +0300

    Merge pull request #17 from NativeScript/mdonev/pre-post-install
    Add preinstall script to enable tslint and firebase configuration out-of-the-box

---
+++ packages/template-master-detail-ng/tools/preinstall.js
@@ -0,0 +1,27 @@
+"use strict";
+const fs = require("fs");
+const path = require("path");
+
+console.log("preinstall script running...");
+
+console.log("creating firebase.nativescript.json to enable firebase...");
+const firebaseConfig = "firebase.nativescript.json";
+copyConfig(firebaseConfig);
+
+console.log("creating tslint.json to enable linting...");
+const tslintConfig = "tslint.json";
+copyConfig(tslintConfig);
+
+function copyConfig(configFilename) {
+    const oldPath = path.join(__dirname, configFilename);
+    const newPath = path.join(getAppRootFolder(), configFilename);
+    fs.rename(oldPath, newPath, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+function getAppRootFolder() {
+    return "../../../";
+}
