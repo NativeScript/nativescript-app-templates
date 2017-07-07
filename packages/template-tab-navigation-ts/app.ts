a1b1a4c0382381d55e7a4ae89f64360130de7a0a
Manol Donev <manoldonev@users.noreply.github.com>
2017-07-07 14:42:48 +0300

    Merge pull request #9 from NativeScript/mdoneb/tslint-support
    Add tslint support

--- packages/template-tab-navigation-ts/app.ts
+++ packages/template-tab-navigation-ts/app.ts
@@ -1,12 +1,12 @@
-﻿/*
+ /*
 In NativeScript, the app.ts file is the entry point to your application.
 You can use this file to perform app-level initialization, but the primary
 purpose of the file is to pass control to the app’s first module.
 */
+import * as app from "application";
 import "./bundle-config";
-import * as app from 'application';
 
-app.start({ moduleName: 'tabs/tabs-page' });
+app.start({ moduleName: "tabs/tabs-page" });
 
 /*
 Do not place any code after the application has been started as it will not
--- packages/template-tab-navigation-ts/bundle-config.ts
+++ packages/template-tab-navigation-ts/bundle-config.ts
@@ -1,8 +1,8 @@
 if ((<any>global).TNS_WEBPACK) {
-    //registers tns-core-modules UI framework modules
+    // registers tns-core-modules UI framework modules
     require("bundle-entry-points");
 
-    //register application modules
+    // register application modules
     global.registerModule("tabs/tabs-page", () => require("./tabs/tabs-page"));
     global.registerModule("browse/Browse", () => require("./browse/BrowseView"));
     global.registerModule("featured/Featured", () => require("./featured/FeaturedView"));
