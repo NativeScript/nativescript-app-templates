03e49997c195fa5cbca07ca2b945411814aff6ab
Manol Donev <manoldonev@users.noreply.github.com>
2017-07-10 19:02:59 +0300

    Merge pull request #11 from NativeScript/mdonev/pre-post-install-update
    Update pre/postinstall scripts

---
+++ packages/template-tab-navigation-ts/tools/postinstall.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const path = require("path");
+const packageJsonPath = path.join(process.cwd(), getAppRootFolder(), "package.json");
+const packageJson = require(packageJsonPath);
+
+console.log("postinstall script running...");
+
+console.log("Updating package.json scripts for linting...");
+// results of glob parameter expansion can vary depending on shell, and its configuration
+// quote the parameter to use node glob syntax (using double quotes if you need it to run in Windows)
+addScriptCommand("lint", "tslint \"app/**/*.ts\"");
+
+// Remove tools folder including this script
+console.log("Removing tools directory...");
+deleteFolder(__dirname);
+
+function addScriptCommand(commandName, command) {
+    if (!packageJson) {
+        console.error(`${packageJsonPath} not found.`);
+
+        return;
+    }
+
+    if (!packageJson.scripts) {
+        packageJson.scripts = {};
+    }
+
+    packageJson.scripts[commandName] = command;
+
+    const updatedContent = JSON.stringify(packageJson);
+    fs.writeFile(packageJsonPath, updatedContent, (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+}
+
+function deleteFolder(folderPath) {
+    if (fs.statSync(folderPath).isDirectory()) {
+        fs.readdirSync(folderPath).forEach((file) => {
+            const content = path.join(folderPath, file);
+            const contentDirs = fs.statSync(content).isDirectory();
+
+            if (contentDirs) {
+                deleteFolder(content);
+            }
+            else {
+                fs.unlinkSync(content);
+            }
+        });
+
+        fs.rmdirSync(folderPath);
+    }
+}
+
+function getAppRootFolder() {
+    return path.join("..", "..");
+}
