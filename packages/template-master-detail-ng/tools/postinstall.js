51596b32f56385ac98b5deda3e4d3f46b8d1cc60
Manol Donev <manoldonev@users.noreply.github.com>
2017-07-10 15:42:20 +0300

    Merge pull request #29 from NativeScript/mdonev/pre-post-install-refactor
    Refactor pre/postinstall scripts

--- packages/template-master-detail-ng/tools/postinstall.js
+++ packages/template-master-detail-ng/tools/postinstall.js
@@ -3,8 +3,12 @@ const path = require("path");
 const packageJsonPath = path.join(process.cwd(), getAppRootFolder(), "package.json");
 const packageJson = require(packageJsonPath);
 
+console.log("postinstall script running...");
+
 console.log("Updating package.json scripts for linting...");
-addLintScriptCommand();
+// results of glob parameter expansion can vary depending on shell, and its configuration
+// quote the parameter to use node glob syntax (using double quotes if you need it to run in Windows)
+addScriptCommand("lint", "tslint \"app/**/*.ts\"");
 
 console.log("Update google services configuration for firebase...");
 updateFirebaseConfigAppId();
@@ -13,7 +17,7 @@ updateFirebaseConfigAppId();
 console.log("Removing tools directory...");
 deleteFolder(__dirname);
 
-function addLintScriptCommand() {
+function addScriptCommand(commandName, command) {
     if (!packageJson) {
         console.error(`${packageJsonPath} not found.`);
 
@@ -24,7 +28,7 @@ function addLintScriptCommand() {
         packageJson.scripts = {};
     }
 
-    packageJson.scripts.lint = "tslint ./app/**/*.ts";
+    packageJson.scripts[commandName] = command;
 
     const updatedContent = JSON.stringify(packageJson);
     fs.writeFile(packageJsonPath, updatedContent, (err) => {
@@ -93,5 +97,5 @@ function deleteFolder(folderPath) {
 }
 
 function getAppRootFolder() {
-    return "../../";
+    return path.join("..", "..");
 }
