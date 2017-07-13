edabc818be8a18c5312dc50530356386f131b71a
Veronika Georgieva <Veronika.Georgieva@telerik.com>
2017-07-12 23:20:09 -0700

    Merge pull request #12 from NativeScript/vegeorgi/styles
    Add template styles

--- packages/template-tab-navigation/tabs/tabs-view-model.js
+++ packages/template-tab-navigation/tabs/tabs-view-model.js
@@ -1,9 +1,11 @@
 const Observable = require("data/observable").Observable;
+const platform = require("platform");
 
 function TabsViewModel() {
     const viewModel = new Observable();
 
     viewModel.title = "";
+    viewModel.iconPath = platform.isAndroid ? "res://" : "res://tabIcons/";
 
     return viewModel;
 }
