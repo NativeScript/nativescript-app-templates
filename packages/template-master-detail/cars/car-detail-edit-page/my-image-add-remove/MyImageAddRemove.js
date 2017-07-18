c256f13dccfdf08cef254930aeb36bfa7fd470df
Martin Yankov <m.i.yankov@gmail.com>
2017-07-18 11:54:18 +0300

    Merge pull request #14 from NativeScript/myankov/prepend-custom-components
    prepend my- to custom components

--- packages/template-master-detail/cars/car-detail-edit-page/my-image-add-remove/MyImageAddRemove.js
+++ packages/template-master-detail/cars/car-detail-edit-page/my-image-add-remove/MyImageAddRemove.js
@@ -1,5 +1,5 @@
 /* ***********************************************************
-* The ImageAddRemove custom component uses an imagepicker plugin to let the user select
+* The MyImageAddRemove custom component uses an imagepicker plugin to let the user select
 * an image and provides custom logic and design to the process.
 *************************************************************/
 function onImageAddRemoveTap(args) {
