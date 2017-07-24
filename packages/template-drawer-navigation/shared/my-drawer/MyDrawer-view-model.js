0ccdc96557633e2ae6d9e758f22e0e406da2afc5
Martin Yankov <m.i.yankov@gmail.com>
2017-07-24 10:17:33 +0300

    Merge pull request #16 from NativeScript/myankov/fix-observable-declaration
    change observable declaration

--- packages/template-drawer-navigation/shared/my-drawer/MyDrawer-view-model.js
+++ packages/template-drawer-navigation/shared/my-drawer/MyDrawer-view-model.js
@@ -1,53 +1,53 @@
-const Observable = require("data/observable").Observable;
+const observableModule = require("data/observable");
 
 /* ***********************************************************
 * Keep data that is displayed in your app drawer in the MyDrawer custom component view model.
 *************************************************************/
 function MyDrawerViewModel(selectedPage) {
-    const viewModel = new Observable();
-
-    /* ***********************************************************
-    * Use the MyDrawer view model to initialize the properties data values.
-    * The navigationItems property is initialized here and is data bound to <ListView> in the MyDrawer view file.
-    * Add, remove or edit navigationItems to change what is displayed in the app drawer list.
-    *************************************************************/
-    viewModel.navigationItems = [
-        {
-            title: "Home",
-            name: "home",
-            route: "home/home-page",
-            icon: "\uf015",
-            isSelected: selectedPage === "Home"
-        },
-        {
-            title: "Browse",
-            name: "browse",
-            route: "browse/browse-page",
-            icon: "\uf1ea",
-            isSelected: selectedPage === "Browse"
-        },
-        {
-            title: "Search",
-            name: "search",
-            route: "search/search-page",
-            icon: "\uf002",
-            isSelected: selectedPage === "Search"
-        },
-        {
-            title: "Featured",
-            name: "featured",
-            route: "featured/featured-page",
-            icon: "\uf005",
-            isSelected: selectedPage === "Featured"
-        },
-        {
-            title: "Settings",
-            name: "settings",
-            route: "settings/settings-page",
-            icon: "\uf013",
-            isSelected: selectedPage === "Settings"
-        }
-    ];
+    const viewModel = observableModule.fromObject({
+        /* ***********************************************************
+        * Use the MyDrawer view model to initialize the properties data values.
+        * The navigationItems property is initialized here and is data bound to <ListView> in the MyDrawer view file.
+        * Add, remove or edit navigationItems to change what is displayed in the app drawer list.
+        *************************************************************/
+        navigationItems: [
+            {
+                title: "Home",
+                name: "home",
+                route: "home/home-page",
+                icon: "\uf015",
+                isSelected: selectedPage === "Home"
+            },
+            {
+                title: "Browse",
+                name: "browse",
+                route: "browse/browse-page",
+                icon: "\uf1ea",
+                isSelected: selectedPage === "Browse"
+            },
+            {
+                title: "Search",
+                name: "search",
+                route: "search/search-page",
+                icon: "\uf002",
+                isSelected: selectedPage === "Search"
+            },
+            {
+                title: "Featured",
+                name: "featured",
+                route: "featured/featured-page",
+                icon: "\uf005",
+                isSelected: selectedPage === "Featured"
+            },
+            {
+                title: "Settings",
+                name: "settings",
+                route: "settings/settings-page",
+                icon: "\uf013",
+                isSelected: selectedPage === "Settings"
+            }
+        ]
+    });
 
     return viewModel;
 }
