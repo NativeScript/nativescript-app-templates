4326be0190d498b477ec36f6eb014df9c9f74983
Martin Yankov <m.i.yankov@gmail.com>
2017-08-02 11:41:38 +0300

    Merge pull request #20 from NativeScript/myankov/page-init
    improve ux through page init changes

--- packages/template-tab-navigation-ts/tabs/tabs-page.ts
+++ packages/template-tab-navigation-ts/tabs/tabs-page.ts
@@ -1,19 +1,24 @@
-import { EventData } from "data/observable";
 import { SelectedIndexChangedEventData, TabView, TabViewItem } from "tns-core-modules/ui/tab-view";
-import { Page } from "ui/page";
+import { NavigatedData, Page } from "ui/page";
 
 import { TabsViewModel } from "./tabs-view-model";
 
-const viewModel = new TabsViewModel();
-
-export function onNavigatingTo(args: EventData) {
+/* ***********************************************************
+* Use the "onNavigatingTo" handler to initialize data for the whole tab
+* navigation layout as a whole.
+*************************************************************/
+export function onNavigatingTo(args: NavigatedData) {
     /* ***********************************************************
-    * Use the "onNavigatingTo" handler to initialize data for the whole tab
-    * navigation layout as a whole.
+    * The "onNavigatingTo" event handler lets you detect if the user navigated with a back button.
+    * Skipping the re-initialization on back navigation means the user will see the
+    * page in the same data state that he left it in before navigating.
     *************************************************************/
+    if (args.isBackNavigation) {
+        return;
+    }
 
     const page = <Page>args.object;
-    page.bindingContext = viewModel;
+    page.bindingContext = new TabsViewModel();
 }
 
 /* ***********************************************************
@@ -23,7 +28,8 @@ export function onNavigatingTo(args: EventData) {
 *************************************************************/
 export function onSelectedIndexChanged(args: SelectedIndexChangedEventData) {
     const tabView = <TabView>args.object;
+    const bindingContext = <TabsViewModel>tabView.bindingContext;
     const selectedTabViewItem = tabView.items[args.newIndex];
 
-    viewModel.title = selectedTabViewItem.title;
+    bindingContext.title = selectedTabViewItem.title;
 }
