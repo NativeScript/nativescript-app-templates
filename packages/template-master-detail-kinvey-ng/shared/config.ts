0658fa879df2f5323dd2ff2605a28f86257c361d
Alexander Djenkov <a.djenkov90@gmail.com>
2017-08-02 11:58:41 +0300

    Merge pull request #5 from NativeScript/djenkov/setup-article
    Update README and add Kinvey database setup article

--- packages/template-master-detail-kinvey-ng/shared/config.ts
+++ packages/template-master-detail-kinvey-ng/shared/config.ts
@@ -1,6 +1,6 @@
 export class Config {
-    static kinveyUsername = "root";
-    static kinveyPassword = "root";
     static kinveyAppKey = "kid_Sy8CV9v7W";
     static kinveyAppSecret = "94d5019e0f45412b9dc419f94e019ca0";
+    static kinveyUsername = "admin";
+    static kinveyPassword = "admin";
 }
