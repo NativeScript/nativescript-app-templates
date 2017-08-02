93952d08984bd091b5043d7da42a2c23cdc01d2a
Alexander Djenkov <a.djenkov90@gmail.com>
2017-08-02 11:00:29 +0300

    Merge pull request #10 from NativeScript/djenkov/fix-event
    Fix wrong navigatingTo event handler name

--- packages/template-blank-ts/home/home-page.ts
+++ packages/template-blank-ts/home/home-page.ts
@@ -1,20 +1,12 @@
-/*
-In NativeScript, a file with the same name as an XML file is known as
-a code-behind file. The code-behind is a great place to place your view
-logic, and to set up your page’s data binding.
-*/
-
-import { EventData } from 'data/observable';
 import { StackLayout } from 'ui/layouts/stack-layout';
+import { NavigatedData } from 'ui/page';
+
 import { HomeViewModel } from './home-view-model';
 
-// Event handler for Page "navigatingTo" event attached in tabs-page.xml
-export function navigatingTo(args: EventData) {
-    /*
-    This gets a reference this page’s <StackLayout> UI component. You can
-    view the API reference of the Page to see what’s available at
-    https://docs.nativescript.org/api-reference/classes/_ui_page_.page.html
-    */
+/* ***********************************************************
+* Use the "onNavigatingTo" handler to initialize the page binding context.
+*************************************************************/
+export function onNavigatingTo(args: NavigatedData) {
     let page = <StackLayout>args.object;
     page.bindingContext = new HomeViewModel();
 }
