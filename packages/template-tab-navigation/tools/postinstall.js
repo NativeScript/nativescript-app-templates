09303d36a669a40be6b8103ba6af8252e18564bb
Manol Donev <manoldonev@users.noreply.github.com>
2017-08-04 11:47:43 +0300

    Merge pull request #18 from NativeScript/mdonev/npm5-fix
    Update pre/postinstall scripts to work with npm5

--- packages/template-tab-navigation/tools/postinstall.js
+++ packages/template-tab-navigation/tools/postinstall.js
@@ -1,48 +1,90 @@
 const fs = require("fs");
 const path = require("path");
-const packageJsonPath = path.join(process.cwd(), getAppRootFolder(), "package.json");
-const packageJson = require(packageJsonPath);
+const exec = require("child_process").exec;
 
 console.log("postinstall script running...");
 
-console.log("Updating package.json scripts for linting...");
-// results of glob parameter expansion can vary depending on shell, and its configuration
-// quote the parameter to use node glob syntax (using double quotes if you need it to run in Windows)
-addScriptCommand("lint", "eslint \"app/**/*.js\"");
+getPackageJson()
+    .then((packageJsonData) => {
+        // results of glob parameter expansion can vary depending on shell, and its configuration
+        // quote the parameter to use node glob syntax (using double quotes if you need it to run in Windows)
+        addScriptCommand(packageJsonData, {
+            commandName: "lint",
+            command: "eslint \"app/**/*.js\"",
+            message: "Updating package.json scripts for linting..."
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+    })
+    .then(() => {
+        // Remove tools folder including this script
+        console.log("Removing tools directory...");
+        deleteFolderSync(__dirname);
+    });
 
-// Remove tools folder including this script
-console.log("Removing tools directory...");
-deleteFolder(__dirname);
+function getPackageJson() {
+    return getAppRootFolder()
+        .then((appRootFolder) => {
+            const packageJsonPath = path.join(appRootFolder, "package.json");
+            const packageJson = require(packageJsonPath);
 
-function addScriptCommand(commandName, command) {
-    if (!packageJson) {
-        console.error(`${packageJsonPath} not found.`);
+            if (!packageJson) {
+                throw new Error("package.json file not found");
+            }
 
-        return;
-    }
+            return {
+                packageJson,
+                packageJsonPath
+            };
+        });
+}
 
-    if (!packageJson.scripts) {
-        packageJson.scripts = {};
-    }
+function getAppRootFolder() {
+    return new Promise((resolve, reject) => {
+        // npm prefix returns the closest parent directory to contain a package.json file
+        exec("cd .. && npm prefix", (err, stdout) => {
+            if (err) {
+                return reject(err);
+            }
 
-    packageJson.scripts[commandName] = command;
-
-    const updatedContent = JSON.stringify(packageJson);
-    fs.writeFile(packageJsonPath, updatedContent, (err) => {
-        if (err) {
-            console.error(err);
-        }
+            resolve(stdout.toString().trim());
+        });
     });
 }
 
-function deleteFolder(folderPath) {
+function addScriptCommand(packageJsonData, options) {
+    return new Promise((resolve, reject) => {
+        if (options.message) {
+            console.log(options.message);
+        }
+
+        const { packageJson, packageJsonPath } = packageJsonData;
+        if (!packageJson.scripts) {
+            packageJson.scripts = {};
+        }
+
+        packageJson.scripts[options.commandName] = options.command;
+
+        const updatedContent = JSON.stringify(packageJson);
+        fs.writeFile(packageJsonPath, updatedContent, (err) => {
+            if (err) {
+                return reject(err);
+            }
+
+            resolve();
+        });
+    });
+}
+
+function deleteFolderSync(folderPath) {
     if (fs.statSync(folderPath).isDirectory()) {
         fs.readdirSync(folderPath).forEach((file) => {
             const content = path.join(folderPath, file);
             const contentDirs = fs.statSync(content).isDirectory();
 
             if (contentDirs) {
-                deleteFolder(content);
+                deleteFolderSync(content);
             }
             else {
                 fs.unlinkSync(content);
@@ -51,8 +93,4 @@ function deleteFolder(folderPath) {
 
         fs.rmdirSync(folderPath);
     }
-}
-
-function getAppRootFolder() {
-    return path.join("..", "..");
 }
