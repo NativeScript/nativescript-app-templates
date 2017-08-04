1be0ca3f42b6e319c22880763c035cbe095ac300
Manol Donev <manoldonev@users.noreply.github.com>
2017-08-04 11:48:06 +0300

    Merge pull request #26 from NativeScript/mdonev/npm5-fix
    Update pre/postinstall scripts to work with npm5

--- packages/template-master-detail-ts/tools/preinstall.js
+++ packages/template-master-detail-ts/tools/preinstall.js
@@ -1,26 +1,43 @@
 const fs = require("fs");
 const path = require("path");
+const exec = require("child_process").exec;
 
 console.log("preinstall script running...");
 
 const firebaseConfig = "firebase.nativescript.json";
-console.log(`creating ${firebaseConfig} to enable firebase...`);
-copyConfig(firebaseConfig);
-
 const tslintConfig = "tslint.json";
-console.log(`creating ${tslintConfig} to enable linting...`);
-copyConfig(tslintConfig);
 
-function copyConfig(configFilename) {
-    const oldPath = path.join(__dirname, configFilename);
-    const newPath = path.join(getAppRootFolder(), configFilename);
-    fs.rename(oldPath, newPath, (err) => {
-        if (err) {
-            console.log(err);
-        }
+getAppRootFolder()
+    .then((appRootFolder) => Promise.all([
+        copyConfig(firebaseConfig, appRootFolder),
+        copyConfig(tslintConfig, appRootFolder)
+    ]));
+
+function copyConfig(configFilename, appRootFolder) {
+    return new Promise((resolve, reject) => {
+        const sourcePath = path.join(__dirname, configFilename);
+        const destPath = path.join(appRootFolder, configFilename);
+
+        console.log(`creating ${path.resolve(destPath)}...`);
+        fs.rename(sourcePath, destPath, (err) => {
+            if (err) {
+                return reject(err);
+            }
+
+            resolve();
+        });
     });
 }
 
 function getAppRootFolder() {
-    return path.join("..", "..", "..");
+    return new Promise((resolve, reject) => {
+        // npm prefix returns the closest parent directory to contain a package.json file
+        exec("cd .. && npm prefix", (err, stdout) => {
+            if (err) {
+                return reject(err);
+            }
+
+            resolve(stdout.toString().trim());
+        });
+    });
 }
