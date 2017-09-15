feb14b60fff4008047573ca4266813b0414b1532
Manol Donev <manoldonev@users.noreply.github.com>
2017-09-15 13:53:02 +0300

    Merge pull request #16 from NativeScript/mdonev/required-field-validation
    Add required field validation on edit form

--- packages/template-master-detail-kinvey-ts/cars/shared/car-model.ts
+++ packages/template-master-detail-kinvey-ts/cars/shared/car-model.ts
@@ -5,7 +5,6 @@ import { ObservableProperty } from "../../shared/observable-property-decorator";
 export class Car extends Observable {
     @ObservableProperty() id: string;
     @ObservableProperty() imageStoragePath: string;
-    @ObservableProperty() name: string;
     @ObservableProperty() seats: string;
     @ObservableProperty() luggage: number;
     @ObservableProperty() class: string;
@@ -13,13 +12,18 @@ export class Car extends Observable {
     @ObservableProperty() doors: number;
     @ObservableProperty() price: number;
     @ObservableProperty() transmission: string;
-    @ObservableProperty() imageUrl: string;
+    @ObservableProperty() isModelValid: boolean;
+
+    private _name: string;
+    private _imageUrl: string;
 
     constructor(options: any) {
         super();
 
+        this._name = options.name;
+        this._imageUrl = options.imageUrl;
+
         this.id = options.id;
-        this.name = options.name;
         this.seats = options.seats;
         this.luggage = Number(options.luggage);
         this.class = options.class;
@@ -27,7 +31,42 @@ export class Car extends Observable {
         this.doors = Number(options.doors);
         this.price = Number(options.price);
         this.transmission = options.transmission;
-        this.imageUrl = encodeURI(options.imageUrl);
         this.imageStoragePath = options.imageStoragePath;
+
+        this.updateDependentProperties();
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    set name(value: string) {
+        if (this._name === value) {
+            return;
+        }
+
+        this._name = value;
+        this.notifyPropertyChange("name", value);
+
+        this.updateDependentProperties();
+    }
+
+    get imageUrl(): string {
+        return this._imageUrl;
+    }
+
+    set imageUrl(value: string) {
+        if (this._imageUrl === value) {
+            return;
+        }
+
+        this._imageUrl = value;
+        this.notifyPropertyChange("imageUrl", value);
+
+        this.updateDependentProperties();
+    }
+
+    private updateDependentProperties(): void {
+        this.isModelValid = !!this._name && !!this._imageUrl;
     }
 }
