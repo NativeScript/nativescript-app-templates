a26337b5a78da2f4d71c9bfb215170ed5f2918f1
Alexander Djenkov <a.djenkov90@gmail.com>
2017-10-09 17:46:55 +0300

    Merge pull request #59 from beeman/beeman/update-telerik-ui-package
    Change dependency nativescript-telerik-ui to nativescript-pro-ui

--- packages/template-drawer-navigation-ng/search/search.component.ts
+++ packages/template-drawer-navigation-ng/search/search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
-import { DrawerTransitionBase, SlideInOnTopTransition } from "nativescript-telerik-ui/sidedrawer";
-import { RadSideDrawerComponent } from "nativescript-telerik-ui/sidedrawer/angular";
+import { DrawerTransitionBase, SlideInOnTopTransition } from "nativescript-pro-ui/sidedrawer";
+import { RadSideDrawerComponent } from "nativescript-pro-ui/sidedrawer/angular";
 
 @Component({
     selector: "Search",
--- packages/template-drawer-navigation-ng/shared/shared.module.ts
+++ packages/template-drawer-navigation-ng/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import { NativeScriptUISideDrawerModule } from "nativescript-telerik-ui/sidedrawer/angular";
+import { NativeScriptUISideDrawerModule } from "nativescript-pro-ui/sidedrawer/angular";
 
 import { MyDrawerComponent } from "./my-drawer/my-drawer.component";
 
