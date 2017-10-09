40d687472a973c4e17da3e7bb25b4f039bfeae87
Alexander Djenkov <a.djenkov90@gmail.com>
2017-10-09 14:03:24 +0300

    Merge pull request #27 from NativeScript/djenkov/ns-ui-update
    Change dependency nativescript-telerik-ui to nativescript-pro-ui

--- packages/template-master-detail-kinvey-ts/bundle-config.ts
+++ packages/template-master-detail-kinvey-ts/bundle-config.ts
@@ -2,8 +2,8 @@ if (global.TNS_WEBPACK) {
     // registers tns-core-modules UI framework modules
     require("bundle-entry-points");
 
-    global.registerModule("nativescript-telerik-ui/listview",
-        () => require("../node_modules/nativescript-telerik-ui/listview"));
+    global.registerModule("nativescript-pro-ui/listview",
+        () => require("../node_modules/nativescript-pro-ui/listview"));
 
     // register application modules
     global.registerModule("cars/cars-list-page", () => require("./cars/cars-list-page"));
