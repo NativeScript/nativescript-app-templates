69baed987785266079526519a0d986853df152dd
Manol Donev <manoldonev@users.noreply.github.com>
2017-10-16 13:58:23 +0300

    Merge pull request #47 from NativeScript/mdonev/simplify-content
    Simplify drawer content

--- packages/template-drawer-navigation-ts/shared/my-drawer/MyDrawer-view-model.ts
+++ packages/template-drawer-navigation-ts/shared/my-drawer/MyDrawer-view-model.ts
@@ -1,59 +1,19 @@
 import { Observable } from "data/observable";
 
+import { ObservableProperty } from "../../shared/observable-property-decorator";
+
 /* ***********************************************************
 * Keep data that is displayed in your app drawer in the MyDrawer custom component view model.
 *************************************************************/
 export class MyDrawerViewModel extends Observable {
-    private _navigationItems: Array<any>;
+    @ObservableProperty() selectedPage: string;
 
     /* ***********************************************************
     * Use the MyDrawer view model constructor to initialize the properties data values.
-    * The navigationItems property is initialized here and is data bound to <ListView> in the MyDrawer view file.
-    * Add, remove or edit navigationItems to change what is displayed in the app drawer list.
     *************************************************************/
     constructor(selectedPage: string) {
         super();
 
-        this._navigationItems = [
-            {
-                title: "Home",
-                name: "home",
-                route: "home/home-page",
-                icon: "\uf015",
-                isSelected: selectedPage === "Home"
-            },
-            {
-                title: "Browse",
-                name: "browse",
-                route: "browse/browse-page",
-                icon: "\uf1ea",
-                isSelected: selectedPage === "Browse"
-            },
-            {
-                title: "Search",
-                name: "search",
-                route: "search/search-page",
-                icon: "\uf002",
-                isSelected: selectedPage === "Search"
-            },
-            {
-                title: "Featured",
-                name: "featured",
-                route: "featured/featured-page",
-                icon: "\uf005",
-                isSelected: selectedPage === "Featured"
-            },
-            {
-                title: "Settings",
-                name: "settings",
-                route: "settings/settings-page",
-                icon: "\uf013",
-                isSelected: selectedPage === "Settings"
-            }
-        ];
-    }
-
-    get navigationItems(): Array<any> {
-        return this._navigationItems;
+        this.selectedPage = selectedPage;
     }
 }
--- packages/template-drawer-navigation-ts/shared/my-drawer/MyDrawer.ts
+++ packages/template-drawer-navigation-ts/shared/my-drawer/MyDrawer.ts
@@ -1,7 +1,6 @@
 import { EventData } from "data/observable";
 import { topmost } from "ui/frame";
-import { StackLayout } from "ui/layouts/stack-layout";
-import { ItemEventData } from "ui/list-view";
+import { GridLayout } from "ui/layouts/grid-layout";
 
 import { MyDrawerViewModel } from "./MyDrawer-view-model";
 
@@ -9,22 +8,23 @@ import { MyDrawerViewModel } from "./MyDrawer-view-model";
 * Use the "loaded" event handler of the wrapping layout element to bind the view model to your view.
 *************************************************************/
 export function onLoaded(args: EventData): void {
-    const component = <StackLayout>args.object;
+    const component = <GridLayout>args.object;
     const componentTitle = component.get("selectedPage");
 
     component.bindingContext = new MyDrawerViewModel(componentTitle);
 }
 
 /* ***********************************************************
-* Use the "itemTap" event handler of the <ListView> component for handling list item taps.
-* The "itemTap" event handler of the app drawer <ListView> is used to navigate the app
+* Use the "tap" event handler of the <GridLayout> component for handling navigation item taps.
+* The "tap" event handler of the app drawer <GridLayout> item is used to navigate the app
 * based on the tapped navigationItem's route.
 *************************************************************/
-export function onNavigationItemTap(args: ItemEventData): void {
-    const navigationItem = args.view.bindingContext;
+export function onNavigationItemTap(args: EventData): void {
+    const component = <GridLayout>args.object;
+    const componentRoute = component.get("route");
 
     topmost().navigate({
-        moduleName: navigationItem.route,
+        moduleName: componentRoute,
         transition: {
             name: "fade"
         }
