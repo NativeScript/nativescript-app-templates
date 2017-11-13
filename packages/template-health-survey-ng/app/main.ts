5c0a51d436135f0b06c5db3c5850c7fbea39e365
Martin Yankov <m.i.yankov@gmail.com>
2017-11-13 10:49:31 +0200

    Init kinvey sdk

--- packages/template-health-survey-ng/app/main.ts
+++ packages/template-health-survey-ng/app/main.ts
@@ -3,4 +3,6 @@ import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 
 import { AppModule } from "./app.module";
 
+import "./shared/kinvey.common";
+
 platformNativeScriptDynamic().bootstrapModule(AppModule);
