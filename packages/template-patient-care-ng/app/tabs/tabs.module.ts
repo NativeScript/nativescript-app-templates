2e3330c96f37b1338804ea774b5e4f307217d5d5
mdonev <manol.donev@gmail.com>
2017-11-14 14:10:07 +0200

    Add missing ng gauge module import

--- packages/template-patient-care-ng/app/tabs/tabs.module.ts
+++ packages/template-patient-care-ng/app/tabs/tabs.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
+import { NativeScriptUIGaugesModule } from "nativescript-pro-ui/gauges/angular";
 import { NativeScriptUIListViewModule } from "nativescript-pro-ui/listview/angular";
 
 import { CareCardComponent } from "./care-card/care-card.component";
@@ -11,6 +12,7 @@ import { TabsComponent } from "./tabs.component";
 @NgModule({
     imports: [
         NativeScriptCommonModule,
+        NativeScriptUIGaugesModule,
         NativeScriptUIListViewModule,
         TabsRoutingModule
     ],
