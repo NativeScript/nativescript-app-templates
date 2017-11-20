7b1c880726a36ae734f68fba41841c72cdcb1f36
mdonev <manol.donev@gmail.com>
2017-11-20 18:54:29 +0200

    Polishing care dashboard component

--- packages/template-patient-care-ng/app/tabs/care-card/care-dashboard/care-dashboard.component.ts
+++ packages/template-patient-care-ng/app/tabs/care-card/care-dashboard/care-dashboard.component.ts
@@ -7,11 +7,75 @@ import { Component, OnInit } from "@angular/core";
 })
 export class CareDashboardComponent implements OnInit {
 
-    value: number = 30;
-
-    values: Array<number> = [10, 20, 70, 100, 100, 0, 0];
+    careState: Array<any>;
+    selectedState: any;
 
     ngOnInit(): void {
-        // OnInit
+        this.careState = this.getCareState();
+        this.selectedState = this.careState[new Date().getDay()];
+    }
+
+    onItemTap(itemDate: Date): void {
+        this.selectedState = this.careState[itemDate.getDay()];
+    }
+
+    // TODO: Extract when we have an idea of the CareKit Kinvey backend
+    private getCareState(): Array<any> {
+        const sunday = this.getLastSunday(new Date());
+        const monday = new Date(sunday);
+        monday.setDate(sunday.getDate() + 1);
+
+        const tuesday = new Date(sunday);
+        tuesday.setDate(sunday.getDate() + 2);
+
+        const wednesday = new Date(sunday);
+        wednesday.setDate(sunday.getDate() + 3);
+
+        const thursday = new Date(sunday);
+        thursday.setDate(sunday.getDate() + 4);
+
+        const friday = new Date(sunday);
+        friday.setDate(sunday.getDate() + 5);
+
+        const saturday = new Date(sunday);
+        saturday.setDate(sunday.getDate() + 6);
+
+        return [
+            {
+                date: sunday,
+                value: 10
+            },
+            {
+                date: monday,
+                value: 20
+            },
+            {
+                date: tuesday,
+                value: 70
+            },
+            {
+                date: wednesday,
+                value: 100
+            },
+            {
+                date: thursday,
+                value: 100
+            },
+            {
+                date: friday,
+                value: 0
+            },
+            {
+                date: saturday,
+                value: 0
+            }
+        ];
+    }
+
+    private getLastSunday(date: Date): Date {
+        const result = new Date(date);
+        result.setDate(date.getDate() - date.getDay());
+
+        return result;
     }
 }
--- packages/template-patient-care-ng/app/tabs/care-card/care-dashboard/circular-status/circular-status.component.ts
+++ packages/template-patient-care-ng/app/tabs/care-card/care-dashboard/circular-status/circular-status.component.ts
@@ -1,6 +1,9 @@
-import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChange, SimpleChanges, ViewChild } from "@angular/core";
-import { View } from "ui/core/view";
+import { Component, ElementRef, Input, OnChanges, SimpleChange, SimpleChanges, ViewChild } from "@angular/core";
+import { Animation } from "ui/animation/animation";
 import { AnimationCurve } from "ui/enums";
+import { Label } from "ui/label";
+
+const radialAnimationDurationMilliseconds = 1000;
 
 @Component({
     selector: "CircularStatus",
@@ -8,66 +11,86 @@ import { AnimationCurve } from "ui/enums";
     templateUrl: "./circular-status.component.html",
     styleUrls: ["./circular-status.component.css"]
 })
-export class CircularStatusComponent implements OnInit, OnChanges {
-    @ViewChild("label") labelElement: ElementRef;
-    @ViewChild("button") buttonElement: ElementRef;
+export class CircularStatusComponent implements OnChanges {
 
+    @ViewChild("progressLabel") progressLabelElement: ElementRef;
+    @ViewChild("completionLabel") completionLabelElement: ElementRef;
+
+    @Input() kFontSize: number;
     @Input() kCol: number;
     @Input() value: number;
     @Input() inProgressIcon: string;
     @Input() completionIcon: string;
 
-    private _label: View;
-    private _button: View;
+    private _progresslabel: Label;
+    private _completionLabel: Label;
+    private _animation: Animation;
 
-    ngOnInit(): void {
-        // this.updateStatusIcon();
+    get radialAnimationDuration(): number {
+        return radialAnimationDurationMilliseconds;
     }
 
-    get label(): View {
-        if (!this._label) {
-            this._label = <View>(this.labelElement.nativeElement);
+    private get progressLabel(): Label {
+        if (!this._progresslabel) {
+            this._progresslabel = <Label>(this.progressLabelElement.nativeElement);
         }
 
-        return this._label;
+        return this._progresslabel;
     }
 
-    get button(): View {
-        if (!this._button) {
-            this._button = <View>(this.buttonElement.nativeElement);
+    private get completionLabel(): Label {
+        if (!this._completionLabel) {
+            this._completionLabel = <Label>(this.completionLabelElement.nativeElement);
         }
 
-        return this._button;
+        return this._completionLabel;
+    }
+
+    private get animation(): Animation {
+        if (!this._animation) {
+            this._animation = new Animation([{
+                target: this.progressLabel,
+                opacity: 0,
+                duration: 0,
+                delay: this.radialAnimationDuration
+            }, {
+                target: this.completionLabel,
+                opacity: 1,
+                duration: 200,
+                delay: this.radialAnimationDuration,
+                curve: AnimationCurve.easeIn
+            }]);
+        }
+
+        return this._animation;
     }
 
     ngOnChanges(changes: SimpleChanges): void {
         if (changes.value) {
-            this.updateAnimations(changes.value);
+            this.updateAnimation(changes.value);
+        }
+
+        if (changes.kFontSize) {
+            // TODO: Reconsider this approach
+            // HACK: customizing the "completion" font size results in proper rendering for the small/large case
+            this.completionLabel.style.fontSize = changes.kFontSize.currentValue;
         }
     }
 
-    private updateAnimations(valueChange: SimpleChange): void {
+    private updateAnimation(valueChange: SimpleChange): void {
         const currentValue = Number(valueChange.currentValue || 0);
         const previousValue = Number(valueChange.previousValue || 0);
+
+        if (this.animation.isPlaying) {
+            this.animation.cancel();
+        }
+
         if (currentValue === 100) {
-            const syncDelay = 1000;
-            this.button.animate({
-                opacity: 1,
-                delay: syncDelay,
-                duration: 200,
-                curve: AnimationCurve.easeIn
-            });
-            setTimeout(() => this.label.opacity = 0, syncDelay);
-        } else if (previousValue === 100) {
-            this.button.opacity = 0;
-            this.label.opacity = 1;
-            // this.label.animate({
-            //     opacity: 1,
-            //     duration: 1000,
-            //     curve: AnimationCurve.easeIn
-            // });
+            // catch animation cancelled error (and do nothing)
+            this.animation.play().catch(() => void 0);
         } else {
-            this.button.opacity = 0;
+            this.completionLabel.opacity = 0;
+            this.progressLabel.opacity = 1;
         }
     }
 }
