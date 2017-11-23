58ee56c7c4bb58644b7e058b4ac918e11f7bc478
mdonev <manol.donev@gmail.com>
2017-11-23 18:24:36 +0200

    Move weekly data stub to care card service

--- packages/template-patient-care-ng/app/care/care-card/care-dashboard/care-dashboard.component.ts
+++ packages/template-patient-care-ng/app/care/care-card/care-dashboard/care-dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 
+import { CareCardService } from "../shared/care-card.service";
+
 @Component({
     selector: "CareDashboard",
     moduleId: module.id,
@@ -8,86 +10,31 @@ import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 export class CareDashboardComponent implements OnInit {
     @Output() selectedDateChange = new EventEmitter<Date>();
 
-    careState: Array<any>;
-    selectedState: any;
+    weeklyState: Array<any>;
 
     private _selectedDate: Date;
 
+    constructor(private _careCardService: CareCardService) { }
+
     ngOnInit(): void {
-        this.careState = this.getCareState();
         this.selectedDate = new Date();
+        this.weeklyState = this._careCardService.getWeeklyOverview(this.selectedDate);
     }
 
     onItemTap(itemDate: Date): void {
-        this.selectedState = this.careState[itemDate.getDay()];
+        this.selectedDate = itemDate;
     }
 
     get selectedDate(): Date {
-        return this.selectedState.date;
+        return this._selectedDate;
     }
 
     set selectedDate(date: Date) {
-        this.selectedState = this.careState[date.getDay()];
+        this._selectedDate = date;
         this.selectedDateChange.emit(date);
     }
 
-    // TODO: Extract when we have an idea of the CareKit Kinvey backend
-    private getCareState(): Array<any> {
-        const sunday = this.getLastSunday(new Date());
-        const monday = new Date(sunday);
-        monday.setDate(sunday.getDate() + 1);
-
-        const tuesday = new Date(sunday);
-        tuesday.setDate(sunday.getDate() + 2);
-
-        const wednesday = new Date(sunday);
-        wednesday.setDate(sunday.getDate() + 3);
-
-        const thursday = new Date(sunday);
-        thursday.setDate(sunday.getDate() + 4);
-
-        const friday = new Date(sunday);
-        friday.setDate(sunday.getDate() + 5);
-
-        const saturday = new Date(sunday);
-        saturday.setDate(sunday.getDate() + 6);
-
-        return [
-            {
-                date: sunday,
-                value: 10
-            },
-            {
-                date: monday,
-                value: 20
-            },
-            {
-                date: tuesday,
-                value: 70
-            },
-            {
-                date: wednesday,
-                value: 100
-            },
-            {
-                date: thursday,
-                value: 100
-            },
-            {
-                date: friday,
-                value: 0
-            },
-            {
-                date: saturday,
-                value: 0
-            }
-        ];
-    }
-
-    private getLastSunday(date: Date): Date {
-        const result = new Date(date);
-        result.setDate(date.getDate() - date.getDay());
-
-        return result;
+    get selectedValue(): number {
+        return this.weeklyState[this._selectedDate.getDay()].value;
     }
 }
