14adc9a6a1ac45b4160f90a9494377af112802be
Manol Donev <manoldonev@users.noreply.github.com>
2017-12-06 19:17:24 +0200

    Merge pull request #31 from NativeScript/mdonev/fix-comment
    Fix comment

--- packages/template-master-detail-kinvey/cars/cars-list-page.js
+++ packages/template-master-detail-kinvey/cars/cars-list-page.js
@@ -31,10 +31,10 @@ function onNavigatingTo(args) {
 
 /* ***********************************************************
 * Use the "itemTap" event handler of the <RadListView> to navigate to the
-* item details page. Retrieve a reference for the data item (the id) and pass it
+* item details page. Retrieve a reference for the data item and pass it
 * to the item details page, so that it can identify which data item to display.
-* Learn more about navigating with a parameter in this documentation article:
-* http://docs.nativescript.org/angular/core-concepts/angular-navigation.html#passing-parameter
+* Learn more about navigating and passing context in this documentation article:
+* https://docs.nativescript.org/core-concepts/navigation#navigate-and-pass-context
 *************************************************************/
 function onCarItemTap(args) {
     const tappedCarItem = args.view.bindingContext;
