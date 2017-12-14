2bb3fa8a16e94b168866e00e33567c7d71a1ec0f
Panayot Cankov <Panayot.Cankov@telerik.com>
2017-12-14 18:48:25 +0200

    feat: Let fragment suffixed files to be added to bundle (#71)
    This will let webpacking XMLs containing partial UI declaration, e. g. the SideDrawer side content etc.

--- packages/template-hello-world/bundle-config.js
+++ packages/template-hello-world/bundle-config.js
@@ -4,6 +4,6 @@ if (global.TNS_WEBPACK) {
 
     // register application modules
     // This will register each `page` postfixed xml, css, js, ts, scss etc. in the app/ folder
-    const context = require.context("~/", true, /page\.(xml|css|js|ts|scss|less|sass)$/);
+    const context = require.context("~/", true, /(page|fragment)\.(xml|css|js|ts|scss|less|sass)$/);
     global.registerWebpackModules(context);
 }
