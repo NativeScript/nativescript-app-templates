58b84b461087af550b888f87db33e7ac73dbec55
Alexander Djenkov <a.djenkov90@gmail.com>
2017-12-19 16:31:54 +0200

    Merge pull request #9 from NativeScript/djenkov/kinvey-init
    Extend Kinvey client config

--- packages/template-patient-care-ng/shared/config.ts
+++ packages/template-patient-care-ng/shared/config.ts
@@ -1,4 +1,6 @@
 export class Config {
-    static kinveyAppKey = "kid_BJKRSkZgf";
-    static kinveyAppSecret = "9d6ea58765e64081a519fa82f2dbf482";
+    static kinveyAppKey: string = "kid_BJKRSkZgf";
+    static kinveyAppSecret: string = "9d6ea58765e64081a519fa82f2dbf482";
+    static kinveyApiHostname: string;
+    static kinveyMicHostname: string;
 }
--- packages/template-patient-care-ng/shared/kinvey.common.ts
+++ packages/template-patient-care-ng/shared/kinvey.common.ts
@@ -4,5 +4,7 @@ import { Config } from "./config";
 
 Kinvey.init({
     appKey: Config.kinveyAppKey,
-    appSecret: Config.kinveyAppSecret
+    appSecret: Config.kinveyAppSecret,
+    apiHostname: Config.kinveyApiHostname,
+    micHostname: Config.kinveyMicHostname
 });
