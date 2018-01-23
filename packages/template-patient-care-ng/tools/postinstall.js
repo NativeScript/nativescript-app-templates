237d813abbb7ad71c577446457345e9c32455a08
Alexander Djenkov <a.djenkov90@gmail.com>
2018-01-23 17:49:06 +0200

    Merge pull request #14 from NativeScript/djenkov/web-pack-update
    feat: update Kinvey for webpack and angular ~5.2.0

--- packages/template-patient-care-ng/tools/postinstall.js
+++ packages/template-patient-care-ng/tools/postinstall.js
@@ -5,15 +5,16 @@ const exec = require("child_process").exec;
 console.log("postinstall script running...");
 
 getPackageJson()
-    .then((packageJsonData) => {
+    .then((packageJsonData) => Promise.all([
         // results of glob parameter expansion can vary depending on shell, and its configuration
         // quote the parameter to use node glob syntax (using double quotes if you need it to run in Windows)
         addScriptCommand(packageJsonData, {
             commandName: "lint",
             command: "tslint \"app/**/*.ts\"",
             message: "Updating package.json scripts for linting..."
-        });
-    })
+        }),
+        updateFirebaseConfigAppId(packageJsonData)
+    ]))
     .catch((err) => {
         console.error(err);
     })
@@ -73,6 +74,43 @@ function addScriptCommand(packageJsonData, options) {
             }
 
             resolve();
+        });
+    });
+}
+
+function updateFirebaseConfigAppId(packageJsonData) {
+    console.log("Updating google services configuration for firebase...");
+
+    const googleServicesJsonPath = path.join(process.cwd(), "App_Resources", "Android", "google-services.json");
+    const googleServiceInfoPlistPath = path.join(process.cwd(), "App_Resources", "iOS", "GoogleService-Info.plist");
+
+    const packageJson = packageJsonData.packageJson;
+    if (!packageJson.nativescript || !packageJson.nativescript.id) {
+        return Promise.reject(new Error("cannot find nativescript node in package.json file"));
+    }
+
+    return Promise.all([
+        replaceAppId(googleServicesJsonPath, packageJson.nativescript.id),
+        replaceAppId(googleServiceInfoPlistPath, packageJson.nativescript.id)
+    ]);
+}
+
+function replaceAppId(filePath, appId) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, "utf8", (err, content) => {
+            if (err) {
+                return reject(err);
+            }
+
+            const appIdPlaceholder = "__PACKAGE__";
+            const updatedContent = content.replace(appIdPlaceholder, appId);
+            fs.writeFile(filePath, updatedContent, (err) => {
+                if (err) {
+                    return reject(err);
+                }
+
+                resolve();
+            });
         });
     });
 }
