b46b7cc82fafb37fb83915fd6b0cea2f5d0078b7
Alexander Djenkov <a.djenkov90@gmail.com>
2018-01-23 17:49:07 +0200

    Merge pull request #15 from NativeScript/djenkov/web-pack-update
    feat: update Kinvey for webpack and angular ~5.2.0

--- packages/template-health-survey-ng/app.module.ts
+++ packages/template-health-survey-ng/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule, NgModuleFactoryLoader, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import { NSModuleFactoryLoader } from "nativescript-angular/router";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -20,7 +19,6 @@ import { LoggedInLazyLoadGuard } from "./logged-in-lazy-load.guard";
         AppComponent
     ],
     providers: [
-        { provide: NgModuleFactoryLoader, useClass: NSModuleFactoryLoader },
         LoggedInLazyLoadGuard
     ],
     schemas: [
