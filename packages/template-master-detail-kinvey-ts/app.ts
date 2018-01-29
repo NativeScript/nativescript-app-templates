f8246ccc31448ad1e9bd26e74ade8c32f44a92c9
Vasil Chimev <Vasil.Chimev@telerik.com>
2018-01-29 16:35:20 +0200

    Merge pull request #42 from NativeScript/vchimev/ns4
    feat: add frame as root

--- packages/template-master-detail-kinvey-ts/app.ts
+++ packages/template-master-detail-kinvey-ts/app.ts
@@ -9,7 +9,7 @@ import "./bundle-config";
 *************************************************************/
 import "./shared/kinvey.common";
 
-app.start({ moduleName: "cars/cars-list-page" });
+app.run({ moduleName: "app-root" });
 
 /*
 Do not place any code after the application has been started as it will not
