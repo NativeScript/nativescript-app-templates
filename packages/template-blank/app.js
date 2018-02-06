9aabb84c7bb1a9ca2c61993f460dc1ce43c7b571
Alexander Djenkov <a.djenkov90@gmail.com>
2018-02-06 17:50:46 +0200

    Merge pull request #31 from NativeScript/djenkov/ns-4.0
    chore: update to {N} 4.0 (nested frames)

--- packages/template-blank/app.js
+++ packages/template-blank/app.js
@@ -7,7 +7,7 @@ purpose of the file is to pass control to the app’s first module.
 require("./bundle-config");
 const application = require("application");
 
-application.start({ moduleName: "home/home-page" });
+application.run({ moduleName: "app-root" });
 
 /*
 Do not place any code after the application has been started as it will not
