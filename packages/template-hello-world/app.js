f3b5942b8272530de2dc5edcdcece6892eeaee6e
Alexander Djenkov <a.djenkov90@gmail.com>
2018-02-07 13:14:09 +0200

    chore: update to {N} 4.0 (nested frames) (#74)

--- packages/template-hello-world/app.js
+++ packages/template-hello-world/app.js
@@ -7,7 +7,7 @@ purpose of the file is to pass control to the app’s first module.
 require("./bundle-config");
 var application = require("application");
 
-application.start({ moduleName: "main-page" });
+application.run({ moduleName: "app-root" });
 
 /*
 Do not place any code after the application has been started as it will not
