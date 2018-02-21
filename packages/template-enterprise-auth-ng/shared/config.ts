cf70dbd78c3f7b38edabc12b3346a6a366705dc5
Teodor Bozhikov <Teodor.Bozhikov@progress.com>
2018-02-21 15:41:55 +0200

    chore: remove unused files/resources

--- packages/template-enterprise-auth-ng/shared/config.ts
+++ packages/template-enterprise-auth-ng/shared/config.ts
@@ -1,6 +1,4 @@
 export class Config {
     static kinveyAppKey = "KINVEY_APP_KEY";
     static kinveyAppSecret = "KINVEY_APP_SECRET";
-    static kinveyUsername = "admin";
-    static kinveyPassword = "admin";
 }
