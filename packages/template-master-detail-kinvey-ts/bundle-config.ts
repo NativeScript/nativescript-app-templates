f79025b54155036c829ddb45c0318af362e4535f
Manol Donev <manoldonev@users.noreply.github.com>
2018-03-01 13:20:25 +0200

    chore(deps): migrate to nativescript-ui-listview dependency (#48)

--- packages/template-master-detail-kinvey-ts/bundle-config.ts
+++ packages/template-master-detail-kinvey-ts/bundle-config.ts
@@ -2,8 +2,8 @@ if (global.TNS_WEBPACK) {
     // Register tns-core-modules UI framework modules
     require("bundle-entry-points");
 
-    global.registerModule("nativescript-pro-ui/listview",
-        () => require("../node_modules/nativescript-pro-ui/listview"));
+    global.registerModule("nativescript-ui-listview",
+        () => require("../node_modules/nativescript-ui-listview"));
 
     // Register application modules
     // This will register each `root`, `page`, `fragment` postfixed xml, css, js, ts, scss file in the app/ folder
