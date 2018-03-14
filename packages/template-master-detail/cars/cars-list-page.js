d92b2e286098b60ccf1775e0623558d65e246d3e
Manol Donev <manoldonev@users.noreply.github.com>
2018-03-14 18:12:57 +0200

    fix(rxjs): only active view models should listen for data changes (#74)

--- packages/template-master-detail/cars/cars-list-page.js
+++ packages/template-master-detail/cars/cars-list-page.js
@@ -3,39 +3,47 @@ const topmost = require("ui/frame").topmost;
 const CarsListViewModel = require("./cars-list-view-model");
 
 /* ***********************************************************
-* This is the master list code behind in the master-detail structure.
-* This code behind gets the data, passes it to the master view and displays it in a list.
-* It also handles the navigation to the details page for each item.
-*************************************************************/
+ * This is the master list code behind in the master-detail structure.
+ * This code behind gets the data, passes it to the master view and displays it in a list.
+ * It also handles the navigation to the details page for each item.
+ *************************************************************/
 
 /* ***********************************************************
-* Use the "onNavigatingTo" handler to initialize the page binding context.
-* Call any view model data initialization load here.
-*************************************************************/
+ * Use the "onNavigatingTo" handler to initialize the page binding context.
+ * Call any view model data initialization load here.
+ *************************************************************/
 function onNavigatingTo(args) {
+    const page = args.object;
+
     /* ***********************************************************
-    * The "onNavigatingTo" event handler lets you detect if the user navigated with a back button.
-    * Skipping the re-initialization on back navigation means the user will see the
-    * page in the same data state that he left it in before navigating.
-    *************************************************************/
-    if (args.isBackNavigation) {
-        return;
+     * The "onNavigatingTo" event handler lets you detect if the user navigated with a back button.
+     * Skipping the re-initialization on back navigation means the user will see the
+     * page in the same data state that he left it in before navigating.
+     *************************************************************/
+    let viewModel = page.bindingContext;
+    if (!args.isBackNavigation) {
+        viewModel = new CarsListViewModel();
+        page.bindingContext = viewModel;
     }
 
-    const page = args.object;
-    const viewModel = new CarsListViewModel();
-
-    page.bindingContext = viewModel;
     viewModel.load();
 }
 
+function onNavigatingFrom(args) {
+    const page = args.object;
+    const oldViewModel = page.bindingContext;
+    if (oldViewModel) {
+        oldViewModel.unload();
+    }
+}
+
 /* ***********************************************************
-* Use the "itemTap" event handler of the <RadListView> to navigate to the
-* item details page. Retrieve a reference for the data item and pass it
-* to the item details page, so that it can identify which data item to display.
-* Learn more about navigating and passing context in this documentation article:
-* https://docs.nativescript.org/core-concepts/navigation#navigate-and-pass-context
-*************************************************************/
+ * Use the "itemTap" event handler of the <RadListView> to navigate to the
+ * item details page. Retrieve a reference for the data item and pass it
+ * to the item details page, so that it can identify which data item to display.
+ * Learn more about navigating and passing context in this documentation article:
+ * https://docs.nativescript.org/core-concepts/navigation#navigate-and-pass-context
+ *************************************************************/
 function onCarItemTap(args) {
     const tappedCarItem = args.view.bindingContext;
 
--- packages/template-master-detail/cars/cars-list-view-model.js
+++ packages/template-master-detail/cars/cars-list-view-model.js
@@ -12,16 +12,26 @@ function CarsListViewModel() {
         isLoading: false,
 
         _carService: CarService.getInstance(),
+        _dataSubscription: null,
 
         load: function () {
             this.set("isLoading", true);
 
-            this._carService.load()
-                .finally(() => this.set("isLoading", false))
-                .subscribe((cars) => {
-                    this.set("cars", new ObservableArray(cars));
-                    this.set("isLoading", false);
-                });
+            if (!this._dataSubscription) {
+                this._dataSubscription = this._carService.load()
+                    .finally(() => this.set("isLoading", false))
+                    .subscribe((cars) => {
+                        this.set("cars", new ObservableArray(cars));
+                        this.set("isLoading", false);
+                    });
+            }
+        },
+
+        unload: function() {
+            if (this._dataSubscription) {
+                this._dataSubscription.unsubscribe();
+                this._dataSubscription = null;
+            }
         }
     });
 
