43a9a85c7be5fceb45586f513216415fa1be5b17
Stoyan Stratev <bit@gbg.bg>
2018-03-23 15:17:13 +0200

    Merge pull request #5 from NativeScript/radeva/enable-webpack
    chore: enable webpack

--- packages/template-enterprise-auth-ts/bundle-config.ts
+++ packages/template-enterprise-auth-ts/bundle-config.ts
@@ -3,6 +3,8 @@ if ((global).TNS_WEBPACK) {
     require("bundle-entry-points");
 
     // Register application modules
+    global.registerModule("ui/layouts/wrap-layout", () => require("ui/layouts/wrap-layout"))
+    
     // This will register each `root`, `page`, `fragment` postfixed xml, css, js, ts, scss file in the app/ folder
     const context = require.context("~/", true, /(root|page|fragment)\.(xml|css|js|ts|scss)$/);
     global.registerWebpackModules(context);
