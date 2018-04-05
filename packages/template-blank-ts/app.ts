e6f1afcaaabf0a17b9ee7869b5b49d48e2b1b687
Martin Yankov <m.i.yankov@gmail.com>
2018-04-05 13:09:25 +0300

    chore(): polish for {N} 4.0

--- packages/template-blank-ts/app.ts
+++ packages/template-blank-ts/app.ts
@@ -4,8 +4,8 @@ You can use this file to perform app-level initialization, but the primary
 purpose of the file is to pass control to the app’s first module.
 */
 
-import * as app from "application";
 import "./bundle-config";
+import * as app from "application";
 
 app.run({ moduleName: "app-root" });
 
