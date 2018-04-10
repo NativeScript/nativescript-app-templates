f424e95b3427311b06aa06382e748faedaf660e8
Vasil Chimev <Vasil.Chimev@telerik.com>
2018-04-10 11:02:47 +0300

    fix: import SideDrawer (#95)

--- packages/template-drawer-navigation-ng/app.module.ts
+++ packages/template-drawer-navigation-ng/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, NgModuleFactoryLoader, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -9,8 +10,9 @@ import { AppComponent } from "./app.component";
         AppComponent
     ],
     imports: [
+        AppRoutingModule,
         NativeScriptModule,
-        AppRoutingModule
+        NativeScriptUISideDrawerModule
     ],
     declarations: [
         AppComponent
