e3e3843d029adc9ae88daabac5780ed6c3423d26
Manol Donev <manoldonev@users.noreply.github.com>
2018-04-13 11:25:08 +0300

    feat: add default .gitignore to tns-created apps (#89)

--- packages/template-master-detail-ts/tools/preinstall.js
+++ packages/template-master-detail-ts/tools/preinstall.js
@@ -6,17 +6,20 @@ console.log("Preinstall script running...");
 
 const firebaseConfig = "firebase.nativescript.json";
 const tslintConfig = "tslint.json";
+const srcGitignore = "dot.gitignore";
+const destGitignore = ".gitignore";
 
 getAppRootFolder()
     .then((appRootFolder) => Promise.all([
-        copyConfig(firebaseConfig, appRootFolder),
-        copyConfig(tslintConfig, appRootFolder)
+        copyFile(appRootFolder, firebaseConfig),
+        copyFile(appRootFolder, tslintConfig),
+        copyFile(appRootFolder, srcGitignore, destGitignore)
     ]));
 
-function copyConfig(configFilename, appRootFolder) {
+function copyFile(appRootFolder, srcFilename, destFilename = srcFilename) {
     return new Promise((resolve, reject) => {
-        const sourcePath = path.join(__dirname, configFilename);
-        const destPath = path.join(appRootFolder, configFilename);
+        const sourcePath = path.join(__dirname, srcFilename);
+        const destPath = path.join(appRootFolder, destFilename);
 
         console.log(`Creating ${path.resolve(destPath)}...`);
         fs.rename(sourcePath, destPath, (err) => {
