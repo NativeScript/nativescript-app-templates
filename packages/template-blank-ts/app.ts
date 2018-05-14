b5809ae07f876b1420436042d8051a165ab6e0aa
Vasil Chimev <Vasil.Chimev@gmail.com>
2018-05-14 18:34:50 +0300

    chore: update to webpack4

--- packages/template-blank-ts/app.ts
+++ packages/template-blank-ts/app.ts
@@ -4,7 +4,6 @@ You can use this file to perform app-level initialization, but the primary
 purpose of the file is to pass control to the app’s first module.
 */
 
-import "./bundle-config";
 import * as app from "application";
 
 app.run({ moduleName: "app-root" });
