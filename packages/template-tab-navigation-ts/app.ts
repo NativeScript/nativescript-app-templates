20e5d0d9b763e4d82123416e8c68880881f78d0d
Vasil Chimev <Vasil.Chimev@telerik.com>
2018-05-15 16:23:23 +0300

    chore: update to webpack4 (#57)

--- packages/template-tab-navigation-ts/app.ts
+++ packages/template-tab-navigation-ts/app.ts
@@ -4,7 +4,6 @@ You can use this file to perform app-level initialization, but the primary
 purpose of the file is to pass control to the app’s first module.
 */
 import * as app from "application";
-import "./bundle-config";
 
 app.run({ moduleName: "app-root" });
 
