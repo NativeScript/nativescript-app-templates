ace1d2ad25e150c14d5ff5a97941b7fccaec4230
Manol Donev <manoldonev@users.noreply.github.com>
2018-05-16 17:04:57 +0300

    chore: update bundle-config for latest webpack (#69)

--- packages/template-drawer-navigation/bundle-config.js
+++ packages/template-drawer-navigation/bundle-config.js
@@ -1,13 +1,5 @@
 if (global.TNS_WEBPACK) {
-    // Register tns-core-modules UI framework modules
-    require("bundle-entry-points");
-
-    // Register application modules
+    // Register custom modules
     global.registerModule("nativescript-ui-sidedrawer",
         () => require("../node_modules/nativescript-ui-sidedrawer"));
-
-    // Register application modules
-    // This will register each `root`, `page`, `fragment` postfixed xml, css, js, ts, scss file in the app/ folder
-    const context = require.context("~/", true, /(root|page|fragment)\.(xml|css|js|ts|scss)$/);
-    global.registerWebpackModules(context);
 }
