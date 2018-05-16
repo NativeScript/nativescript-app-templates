dae1e1bd299dcb222e25a4a5cbb6f7d7969347a9
Vasil Chimev <Vasil.Chimev@telerik.com>
2018-05-16 18:02:50 +0300

    chore: update to webpack4 (#59)
    * chore: update to rxjs6/webpack4
    
    * chore: remove rxjs deps

--- packages/template-master-detail-kinvey-ts/bundle-config.ts
+++ packages/template-master-detail-kinvey-ts/bundle-config.ts
@@ -1,12 +1,5 @@
 if (global.TNS_WEBPACK) {
-    // Register tns-core-modules UI framework modules
-    require("bundle-entry-points");
-
+    // Register nativescript-ui-listview module
     global.registerModule("nativescript-ui-listview",
         () => require("../node_modules/nativescript-ui-listview"));
-
-    // Register application modules
-    // This will register each `root`, `page`, `fragment` postfixed xml, css, js, ts, scss file in the app/ folder
-    const context = require.context("~/", true, /(root|page|fragment)\.(xml|css|js|ts|scss)$/);
-    global.registerWebpackModules(context);
 }
