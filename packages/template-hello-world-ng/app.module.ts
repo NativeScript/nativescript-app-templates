a034a9c71039aefcfdc5145a8ec7ae822aa03586
Catalin Zalog <xxxxxcata@yahoo.com>
2018-06-26 19:31:25 +0300

    style: deletes some whitespaces (#80)

--- packages/template-hello-world-ng/app.module.ts
+++ packages/template-hello-world-ng/app.module.ts
@@ -10,7 +10,7 @@ import { ItemDetailComponent } from "./item/item-detail.component";
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
-// Uncomment and add to NgModule imports  if you need to use the HTTP wrapper
+// Uncomment and add to NgModule imports if you need to use the HTTP wrapper
 // import { NativeScriptHttpModule } from "nativescript-angular/http";
 
 @NgModule({
--- packages/template-hello-world-ng/item/items.component.ts
+++ packages/template-hello-world-ng/item/items.component.ts
@@ -11,7 +11,7 @@ import { ItemService } from "./item.service";
 export class ItemsComponent implements OnInit {
     items: Item[];
 
-    // This pattern makes use of Angular’s dependency injection implementation to inject an instance of the ItemService service into this class. 
+    // This pattern makes use of Angular’s dependency injection implementation to inject an instance of the ItemService service into this class.
     // Angular knows about this service because it is included in your app’s main NgModule, defined in app.module.ts.
     constructor(private itemService: ItemService) { }
 
