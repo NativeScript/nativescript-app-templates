ca2a738a7d9248bead8e218faa7334447902cf8a
Nick Iliev <nikolay.iliev@telerik.com>
2018-07-19 09:12:54 +0300

    refactor: Http comment replated with HttpClient comment (#83)

--- packages/template-hello-world-ng/app.module.ts
+++ packages/template-hello-world-ng/app.module.ts
@@ -10,8 +10,8 @@ import { ItemDetailComponent } from "./item/item-detail.component";
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
-// Uncomment and add to NgModule imports if you need to use the HTTP wrapper
-// import { NativeScriptHttpModule } from "nativescript-angular/http";
+// Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
+// import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
 
 @NgModule({
     bootstrap: [
