63bfa53778ff3b3b8b321bd12d99bacea1a67c5b
Vasil Chimev <Vasil.Chimev@telerik.com>
2018-07-23 11:42:17 +0300

    chore: delete bundle-config (#51)
    No longer required since `nativescript-dev-webpack@0.12.0`.

--- packages/template-hello-world-ts/app.ts
+++ packages/template-hello-world-ts/app.ts
@@ -4,7 +4,6 @@ You can use this file to perform app-level initialization, but the primary
 purpose of the file is to pass control to the app’s first module.
 */
 
-import "./bundle-config";
 import * as application from 'application';
 
 application.run({ moduleName: 'app-root' });
