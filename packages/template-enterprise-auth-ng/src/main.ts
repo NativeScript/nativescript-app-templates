ecf930d864f7c3f9b6793aeb47386000e02fadb1
sis0k0 <stanimira.vlaeva@gmail.com>
2018-08-27 14:40:17 +0300

    refactor: add app/ subfolder in src/

--- packages/template-enterprise-auth-ng/src/main.ts
+++ packages/template-enterprise-auth-ng/src/main.ts
@@ -1,13 +1,13 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 
-import { AppModule } from "./app.module";
+import { AppModule } from "./app/app.module";
 
 /* ***********************************************************
 * The {N} Kinvey plugin needs some initialization steps before it is ready
 * for use. Check out the initialization script at /shared/kinvey.common.ts
 * along with more information about it.
 *************************************************************/
-import "./shared/kinvey.common";
+import "./app/shared/kinvey.common";
 
 platformNativeScriptDynamic({ startPageActionBarHidden: true }).bootstrapModule(AppModule);
