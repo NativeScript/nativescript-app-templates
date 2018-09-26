f47d87374fd79ca8b3318e6cd9ae3c8e71efeb33
Stoyan Stratev <bit@gbg.bg>
2018-09-26 16:51:44 +0300

    Merge pull request #49 from NativeScript/lini/fix-playground-preview
    fix: correct tns-core-modules imports

--- packages/template-blank/app/home/home-view-model.js
+++ packages/template-blank/app/home/home-view-model.js
@@ -1,4 +1,4 @@
-const observableModule = require("data/observable");
+const observableModule = require("tns-core-modules/data/observable");
 
 function HomeViewModel() {
     const viewModel = observableModule.fromObject({
