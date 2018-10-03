846e5dd9dda843f2af34be223f07b4c264864d4e
Manol Donev <manoldonev@users.noreply.github.com>
2018-10-03 14:07:38 +0300

    fix: after-createProject hook (#67)

--- packages/template-blank-ng/hooks/after-createProject/after-createProject.js
+++ packages/template-blank-ng/hooks/after-createProject/after-createProject.js
@@ -1,69 +1,51 @@
 const fs = require("fs");
 const path = require("path");
-const exec = require("child_process").exec;
 
-const srcGitignore = "dot.gitignore";
-const destGitignore = ".gitignore";
-let appRootFolder;
+module.exports = function (hookArgs) {
+    const srcGitignore = "dot.gitignore";
+    const destGitignore = ".gitignore";
+    let appRootFolder = hookArgs.projectDir;
 
-findAppRootFolder()
-    .then(() => {
-        return copyFile(srcGitignore, destGitignore);
-    })
-    .then(() => {
-        const toolsDir = path.join(appRootFolder, 'tools');
-        deleteFolderSync(toolsDir);
+    return copyFile(srcGitignore, destGitignore)
+        .then(() => {
+            const toolsDir = path.join(appRootFolder, 'tools');
+            deleteFolderSync(toolsDir);
 
-        deleteFolderSync(__dirname);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-
-function copyFile(srcFilename, destFilename = srcFilename) {
-    return new Promise((resolve, reject) => {
-        const sourcePath = path.join(appRootFolder, 'tools', srcFilename);
-        const destPath = path.join(appRootFolder, destFilename);
-
-        console.log(`Creating ${path.resolve(destPath)}...`);
-        fs.rename(sourcePath, destPath, (err) => {
-            if (err) {
-                return reject(err);
-            }
-
-            resolve();
-        });
-    });
-}
-
-function findAppRootFolder() {
-    return new Promise((resolve, reject) => {
-        // npm prefix returns the closest parent directory to contain a package.json file
-        exec(`cd ${__dirname} && npm prefix`, (err, stdout) => {
-            if (err) {
-                return reject(err);
-            }
-
-            appRootFolder = stdout.toString().trim();
-            resolve();
-        });
-    });
-}
-
-function deleteFolderSync(folderPath) {
-    if (fs.statSync(folderPath).isDirectory()) {
-        fs.readdirSync(folderPath).forEach((file) => {
-            const content = path.join(folderPath, file);
-            const contentDirs = fs.statSync(content).isDirectory();
-
-            if (contentDirs) {
-                deleteFolderSync(content);
-            }
-            else {
-                fs.unlinkSync(content);
-            }
+            deleteFolderSync(__dirname);
+        })
+        .catch((err) => {
+            console.log(err);
         });
 
-        fs.rmdirSync(folderPath);
+    function copyFile(srcFilename, destFilename = srcFilename) {
+        return new Promise((resolve, reject) => {
+            const sourcePath = path.join(appRootFolder, 'tools', srcFilename);
+            const destPath = path.join(appRootFolder, destFilename);
+
+            fs.rename(sourcePath, destPath, (err) => {
+                if (err) {
+                    return reject(err);
+                }
+
+                resolve();
+            });
+        });
+    }
+
+    function deleteFolderSync(folderPath) {
+        if (fs.statSync(folderPath).isDirectory()) {
+            fs.readdirSync(folderPath).forEach((file) => {
+                const content = path.join(folderPath, file);
+                const contentDirs = fs.statSync(content).isDirectory();
+
+                if (contentDirs) {
+                    deleteFolderSync(content);
+                } else {
+                    fs.unlinkSync(content);
+                }
+            });
+
+            fs.rmdirSync(folderPath);
+        }
     }
 }
