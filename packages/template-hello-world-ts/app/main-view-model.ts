6c58ff1feead92d3c735457922514f4327b34df9
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2018-10-19 17:52:27 +0300

    refactor: start using double quotes (#57)
    All other TS/JS {N} projects are using double quotes.

--- packages/template-hello-world-ts/app/main-view-model.ts
+++ packages/template-hello-world-ts/app/main-view-model.ts
@@ -1,4 +1,4 @@
-import { Observable } from 'tns-core-modules/data/observable';
+import { Observable } from "tns-core-modules/data/observable";
 
 export class HelloWorldModel extends Observable {
 
@@ -20,7 +20,7 @@ export class HelloWorldModel extends Observable {
     set message(value: string) {
         if (this._message !== value) {
             this._message = value;
-            this.notifyPropertyChange('message', value)
+            this.notifyPropertyChange("message", value)
         }
     }
 
@@ -31,7 +31,7 @@ export class HelloWorldModel extends Observable {
 
     private updateMessage() {
         if (this._counter <= 0) {
-            this.message = 'Hoorraaay! You unlocked the NativeScript clicker achievement!';
+            this.message = "Hoorraaay! You unlocked the NativeScript clicker achievement!";
         } else {
             this.message = `${this._counter} taps left`;
         }
