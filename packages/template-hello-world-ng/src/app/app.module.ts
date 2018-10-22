0c9c6af2140896269f3b72ea7dd2b938a0988395
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2018-10-22 17:52:35 +0300

    refactor: change `app.routing.ts` to `app-routing.ts` to follow the NG (#93)
    style guide

--- packages/template-hello-world-ng/src/app/app.module.ts
+++ packages/template-hello-world-ng/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import { AppRoutingModule } from "./app.routing";
-import { AppComponent } from "./app.component";
 
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 import { ItemService } from "./item/item.service";
 import { ItemsComponent } from "./item/items.component";
 import { ItemDetailComponent } from "./item/item-detail.component";
