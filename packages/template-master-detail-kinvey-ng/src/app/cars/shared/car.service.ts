960d3af6af6b5d65571d0ed1092548365630acef
Manol Donev <manol.donev@gmail.com>
2019-06-07 15:19:18 +0300

    chore(master-detail-ng): update kinvey to 4.1.0

--- packages/template-master-detail-kinvey-ng/src/app/cars/shared/car.service.ts
+++ packages/template-master-detail-kinvey-ng/src/app/cars/shared/car.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
-import { Kinvey } from "kinvey-nativescript-sdk";
+// TODO: should be imported from kinvey-nativescript-sdk/angular but declaration file is currently missing
+import { DataStoreService, FilesService, UserService, Query } from "kinvey-nativescript-sdk/lib/angular";
 import { File } from "tns-core-modules/file-system";
+
 import { Config } from "../../shared/config";
 import { Car } from "./car.model";
 
@@ -25,45 +27,52 @@ export class CarService {
         return editableProperties.reduce((a, e) => (a[e] = car[e], a), { _id: car.id });
     }
 
-    private allCars: Array<Car> = [];
-    private carsStore = Kinvey.DataStore.collection<any>("cars");
+    private _allCars: Array<Car> = [];
+    private _carsStore = null;
+
+    constructor(
+        dataStoreService: DataStoreService,
+        private _filesService: FilesService,
+        private _userService: UserService) {
+            this._carsStore = dataStoreService.collection("cars");
+        }
 
     getCarById(id: string): Car {
         if (!id) {
             return;
         }
 
-        return this.allCars.filter((car) => {
+        return this._allCars.filter((car) => {
             return car.id === id;
         })[0];
     }
 
     load(): Promise<any> {
         return this.login().then(() => {
-            return this.carsStore.sync();
+            return this._carsStore.sync();
         }).then(() => {
-            const sortByNameQuery = new Kinvey.Query();
+            const sortByNameQuery = new Query();
             sortByNameQuery.ascending("name");
-            const stream = this.carsStore.find(sortByNameQuery);
+            const stream = this._carsStore.find(sortByNameQuery);
 
             return stream.toPromise();
         }).then((data) => {
-            this.allCars = [];
+            this._allCars = [];
             data.forEach((carData: any) => {
                 carData.id = carData._id;
                 const car = new Car(carData);
 
-                this.allCars.push(car);
+                this._allCars.push(car);
             });
 
-            return this.allCars;
+            return this._allCars;
         });
     }
 
     update(carModel: Car): Promise<any> {
         const updateModel = CarService.cloneUpdateModel(carModel);
 
-        return this.carsStore.save(updateModel);
+        return this._carsStore.save(updateModel);
     }
 
     uploadImage(remoteFullPath: string, localFullPath: string): Promise<any> {
@@ -77,12 +86,12 @@ export class CarService {
             public: true
         };
 
-        return Kinvey.Files.upload(imageFile, metadata, { timeout: 2147483647 })
+        return this._filesService.upload(imageFile, metadata, { timeout: 2147483647 })
             .then((uploadedFile: any) => {
-                const query = new Kinvey.Query();
+                const query = new Query();
                 query.equalTo("_id", uploadedFile._id);
 
-                return Kinvey.Files.find(query);
+                return this._filesService.find(query);
             })
             .then((files: Array<any>) => {
                 if (files && files.length) {
@@ -97,10 +106,10 @@ export class CarService {
     }
 
     private login(): Promise<any> {
-        if (!!Kinvey.User.getActiveUser()) {
+        if (!!this._userService.getActiveUser()) {
             return Promise.resolve();
         } else {
-            return Kinvey.User.login(Config.kinveyUsername, Config.kinveyPassword);
+            return this._userService.login(Config.kinveyUsername, Config.kinveyPassword);
         }
     }
 
