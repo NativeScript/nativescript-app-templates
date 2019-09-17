e5330d5f1de7de1df76415900f52c8b3c9fd1dd0
Manol Donev <manoldonev@users.noreply.github.com>
2019-09-17 16:32:19 +0300

    feat(vue): add tab navigation template (#95)

---
+++ packages/template-tab-navigation-vue/app/components/App.vue
@@ -0,0 +1,60 @@
+<template lang="html">
+    <BottomNavigation>
+        <TabStrip>
+            <TabStripItem>
+                <Label text="Home"></Label>
+                <Image src.decode="font://&#xf015;" class="fa t-36"></Image>
+            </TabStripItem>
+            <TabStripItem>
+                <Label text="Browse"></Label>
+                <Image src.decode="font://&#xf1ea;" class="fa t-36"></Image>
+            </TabStripItem>
+            <TabStripItem>
+                <Label text="Search"></Label>
+                <Image src.decode="font://&#xf002;" class="fa t-36"></Image>
+            </TabStripItem>
+        </TabStrip>
+
+        <TabContentItem>
+            <Frame>
+                <Items />
+            </Frame>
+        </TabContentItem>
+
+        <TabContentItem>
+            <Frame>
+                <Browse />
+            </Frame>
+        </TabContentItem>
+
+        <TabContentItem>
+            <Frame>
+                <Search />
+            </Frame>
+        </TabContentItem>
+
+    </BottomNavigation>
+</template>
+
+<script>
+import Items from "./Items.vue";
+import Browse from "./Browse.vue";
+import Search from "./Search.vue";
+
+export default {
+  components: {
+    Items,
+    Browse,
+    Search
+  }
+};
+</script>
+
+<style lang="scss">
+// Start custom common variables
+@import "../app-variables";
+// End custom common variables
+
+// Custom styles
+
+</style>
---
+++ packages/template-tab-navigation-vue/app/components/ItemDetails.vue
@@ -0,0 +1,34 @@
+<template>
+    <Page class="page">
+
+        <ActionBar class="action-bar">
+            <NavigationButton @tap="$navigateBack" android.systemIcon="ic_menu_back"/>
+            <Label class="action-bar-title" :text="item.name"></Label>
+        </ActionBar>
+
+        <GridLayout>
+            <Label class="m-10 h3" :text="item.description" verticalAlignment="top" ></Label>
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+export default {
+    props: ["context"],
+
+    computed: {
+        item() {
+            return this.context || {};
+        }
+    }
+};
+</script>
+
+<style scoped lang="scss">
+// Start custom common variables
+@import "../app-variables";
+// End custom common variables
+
+// Custom styles
+
+</style>
