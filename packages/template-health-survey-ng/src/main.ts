cbc2edcd0b7a6d7b6c998e57dc8b8c8a3d5eed5f
Manol Donev <manol.donev@gmail.com>
2019-11-11 16:49:08 +0200

    chore: remove deprecated health cloud templates

--- packages/template-health-survey-ng/src/main.ts
+++
@@ -1,6 +0,0 @@
-// this import should be first in order to load some required settings (like globals and reflect-metadata)
-import { platformNativeScriptDynamic } from "nativescript-angular/platform";
-
-import { AppModule } from "./app/app.module";
-
-platformNativeScriptDynamic().bootstrapModule(AppModule);
--- packages/template-patient-care-ng/src/care/care-card/shared/care-plan-activity-type.enum.ts
+++
@@ -1,5 +0,0 @@
-export const enum CarePlanActivityType {
-    Activity = 0,
-    Assessment = 1,
-    ReadOnly = 2
-}
