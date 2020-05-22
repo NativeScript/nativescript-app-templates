ebb02db4884c0bbfa070029328b124e5c883213c
rosen-vladimirov <rosen.vladimirov@progress.com>
2020-05-22 16:53:42 +0300

    chore: remove kinvey related templates
    Remove Kinvey related templates as they have been moved to another repository.

--- packages/template-master-detail-kinvey-ts/app/cars/car-detail-edit-page/roundingValueConverter.ts
+++
@@ -1,9 +0,0 @@
-export class RoundingValueConverter {
-    toView(value: number): number {
-        return value;
-    }
-
-    toModel(value: number): number {
-        return Math.round(value);
-    }
-}
