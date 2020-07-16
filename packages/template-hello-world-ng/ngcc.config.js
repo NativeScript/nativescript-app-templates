d4510d344a8e18d7edb94e8eec3ea5c9b47601a7
Nathan Walker <walkerrunpdx@gmail.com>
2020-07-15 21:41:46 -0700

    feat(angular): v10 support

--- packages/template-hello-world-ng/ngcc.config.js
+++ packages/template-hello-world-ng/ngcc.config.js
@@ -1,8 +1,49 @@
+/**
+ * As a convenience we've added several common plugins in case your project ends up using them.
+ * Once the listed plugins have been built and distributed with ng-packagr they can be removed from here safely.
+ */
 module.exports = {
   packages: {
-    "@nativescript/angular": {
+    "nativescript-ui-sidedrawer": {
+      entryPoints: {
+          angular: {
+              override: {
+                  main: "./side-drawer-directives.js",
+                  typings: "./side-drawer-directives.d.ts"
+              },
+              ignoreMissingDependencies: true
+          }
+      },
+      ignorableDeepImportMatchers: [
+        /tns-core-modules\//,
+        /@nativescript\/core\//,
+        /nativescript-angular\//,
+      ]
+    },
+    "nativescript-localize": {
       entryPoints: {
         ".": {
+          override: {
+            main: "./angular.js",
+            typings: "./angular.d.ts",
+          },
+          ignoreMissingDependencies: true,
+        },
+      },
+      ignorableDeepImportMatchers: [
+        /@nativescript\/core\//
+      ]
+    },
+    "nativescript-datetimepicker": {
+      entryPoints: {
+        ".": {
+          override: {
+            main: "./index.js",
+            typings: "./index.d.ts",
+          },
+          ignoreMissingDependencies: true,
+        },
+        "angular": {
           override: {
             main: "./index.js",
             typings: "./index.d.ts",
@@ -13,6 +54,39 @@ module.exports = {
       ignorableDeepImportMatchers: [
         /tns-core-modules\//,
         /@nativescript\/core\//,
+        /@nativescript\/angular\//
+      ]
+    },
+    "nativescript-ui-sidedrawer": {
+      entryPoints: {
+        "angular": {
+          override: {
+            main: "./side-drawer-directives.js",
+            typings: "./side-drawer-directives.d.ts",
+          },
+          ignoreMissingDependencies: true,
+        }
+      },
+      ignorableDeepImportMatchers: [
+        /tns-core-modules\//,
+        /@nativescript\/core\//,
+        /@nativescript\/angular\//
+      ]
+    },
+    "nativescript-ui-listview": {
+      entryPoints: {
+        "angular": {
+          override: {
+            main: "./listview-directives.js",
+            typings: "./listview-directives.d.ts",
+          },
+          ignoreMissingDependencies: true,
+        }
+      },
+      ignorableDeepImportMatchers: [
+        /tns-core-modules\//,
+        /@nativescript\/core\//,
+        /@nativescript\/angular\//
       ]
     }
   }
