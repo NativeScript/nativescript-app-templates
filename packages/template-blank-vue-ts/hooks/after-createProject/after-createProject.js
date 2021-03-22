c651ffeaa2864e6baf3579a0fa1b5b6dfd0c9ab9
Janos Hrubos <33330538+janoshrubos@users.noreply.github.com>
2021-03-22 16:30:29 +0100

    fix: missing template-blank-vue-ts afterCreate hooks (#184)
    * handle hooks in gitignore
    
    * add after-createProject.js
    
    * Update package.json
    
    Co-authored-by: Igor Randjelovic <rigor789@gmail.com>

---
+++ packages/template-blank-vue-ts/hooks/after-createProject/after-createProject.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = function (hookArgs) {
+    const appRootFolder = hookArgs.projectDir;
+    const toolsDir = path.join(appRootFolder, "tools");
+    const vscodeDir = path.join(appRootFolder, ".vscode");
+    const srcGitignore = path.join(toolsDir, "dot.gitignore");
+    const destGitignore = path.join(appRootFolder, ".gitignore");
+    const srcVscodeExtensions = path.join(toolsDir, "vscode.extensions.json");
+    const destVscodeExtensions = path.join(vscodeDir, "extensions.json");
+
+    try {
+        fs.mkdirSync(vscodeDir);
+        fs.copyFileSync(srcVscodeExtensions, destVscodeExtensions);
+        fs.copyFileSync(srcGitignore, destGitignore);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        try {
+            deleteFolderSync(toolsDir);
+
+            const readme = path.join(appRootFolder, "README.md");
+            fs.unlinkSync(readme);
+    
+            deleteFolderSync(__dirname);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    function deleteFolderSync(folderPath) {
+        if (fs.statSync(folderPath).isDirectory()) {
+            fs.readdirSync(folderPath).forEach((file) => {
+                const content = path.join(folderPath, file);
+                const contentDirs = fs.statSync(content).isDirectory();
+
+                if (contentDirs) {
+                    deleteFolderSync(content);
+                } else {
+                    fs.unlinkSync(content);
+                }
+            });
+
+            fs.rmdirSync(folderPath);
+        }
+    }
+};
