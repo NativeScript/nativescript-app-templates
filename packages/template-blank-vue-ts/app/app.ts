5516657a70a9445ef0d406d9e39666953054193e
Igor Randjelovic <rigor789@gmail.com>
2021-04-02 21:45:34 +0200

    chore: declare __DEV__

--- packages/template-blank-vue-ts/app/app.ts
+++ packages/template-blank-vue-ts/app/app.ts
@@ -1,6 +1,8 @@
 import Vue from 'nativescript-vue'
 import Home from './components/Home.vue'
 
+declare let __DEV__: boolean;
+
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = !__DEV__
 
