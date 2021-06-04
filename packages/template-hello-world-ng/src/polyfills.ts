edb0763fb759d09b1da20db9afd91f36fd9bde4f
Nathan Walker <walkerrunpdx@gmail.com>
2021-06-03 21:05:51 -0700

    chore: angular 12

---
+++ packages/template-hello-world-ng/src/polyfills.ts
@@ -0,0 +1,5 @@
+import '@nativescript/core/globals';
+import '@nativescript/angular/polyfills';
+import '@nativescript/zone-js/dist/pre-zone-polyfills';
+import 'zone.js/dist/zone.js';
+import '@nativescript/zone-js';
