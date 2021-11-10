96bde2b2b55d7fdd2aa65508b7c6dd257690ee96
halfnelson <david@pershouse.id.au>
2021-11-11 07:36:30 +1000

    feat(svelte): update template to use latest svelte, svelte-native and svelte-loader (#199)
    * feat(svelte): Update template to use latest svelte-native and svelte-loader
    
    * chore: cleanup
    
    Co-authored-by: Igor Randjelovic <rigor789@gmail.com>

--- packages/template-blank-svelte/app/app.ts
+++ packages/template-blank-svelte/app/app.ts
@@ -1,9 +1,10 @@
 /*
 In NativeScript, the app.ts file is the entry point to your application.
 You can use this file to perform app-level initialization, but the primary
-purpose of the file is to pass control to the app’s first module.
+purpose of the file is to pass control to the app’s first page.
 */
 
-import { svelteNative } from 'svelte-native'
+import { svelteNativeNoFrame } from 'svelte-native'
 import App from './App.svelte'
-svelteNative(App, {})
+
+svelteNativeNoFrame(App, {})
