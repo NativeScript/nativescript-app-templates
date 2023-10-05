df6f8966a0a74cfaeca6c42cb8c4b2031ae2f350
Nathan Walker <walkerrunpdx@gmail.com>
2023-10-04 20:55:33 -0700

    feat: flavor specific icons for visionOS

--- prepareTemplates.js
+++ prepareTemplates.js
@@ -5,6 +5,8 @@ const cpy = require('cpy')
 
 const SHARED_MOBILE_PATH = path.resolve(__dirname, './shared-mobile')
 const SHARED_VISION_PATH = path.resolve(__dirname, './shared-vision')
+const SHARED_VISION_ICONS_PATH = path.resolve(__dirname, './shared-vision-flavor-icons')
+const FLAVORS = ['angular', 'react', 'solid', 'svelte', 'vue']
 const PACKAGES_PATH = path.resolve(__dirname, './packages')
 
 const entries = fs.readdirSync(PACKAGES_PATH, {
@@ -30,6 +32,23 @@ function prepareTemplate(templatePath) {
             dot: true,
             parents: true
         })
+        for (const flavor of FLAVORS) {
+            let pathSuffix = `-${flavor}`;
+            if (flavor === 'angular') {
+                pathSuffix = `-ng`;
+            }
+            if (templatePath.indexOf(pathSuffix) > -1) {
+                // Copy flavor specific Front icon into Assets
+                const visionIconPath = path.resolve(templatePath, 'App_Resources', 'visionOS', 'Assets.xcassets', 'AppIcon.solidimagestack');
+                const flavorFrontIconPath = path.resolve(SHARED_VISION_ICONS_PATH, flavor);
+                cpy(`**/*`, visionIconPath, {
+                    cwd: flavorFrontIconPath,
+                    overwrite: false,
+                    dot: true,
+                    parents: true
+                })
+            }      
+        }
     } else {
         // Copy shared resources into template
         cpy(`**/*`, templatePath, {
