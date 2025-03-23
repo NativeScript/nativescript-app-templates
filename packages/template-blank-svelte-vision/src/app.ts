01fdd3fc36bcf97bd94a6e9f80685ec038da92ed
Nathan Walker <walkerrunpdx@gmail.com>
2025-03-22 20:14:56 -0700

    feat: @nativescript-community/svelte-native

--- packages/template-blank-svelte-vision/src/app.ts
+++ packages/template-blank-svelte-vision/src/app.ts
@@ -4,7 +4,7 @@ You can use this file to perform app-level initialization, but the primary
 purpose of the file is to pass control to the app’s first page.
 */
 
-import { svelteNativeNoFrame } from 'svelte-native'
+import { svelteNativeNoFrame } from '@nativescript-community/svelte-native'
 import App from './App.svelte'
 
 svelteNativeNoFrame(App, {})
