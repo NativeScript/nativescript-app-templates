62cc004c2a2971fbdbb5bd3cdddd72a2dd71df77
Nathan Walker <walkerrunpdx@gmail.com>
2025-06-01 12:16:24 -0700

    feat: angular 20 default

--- packages/template-blank-ng/src/main.ts
+++ packages/template-blank-ng/src/main.ts
@@ -1,20 +1,13 @@
 import {
   bootstrapApplication,
   provideNativeScriptHttpClient,
-  provideNativeScriptNgZone,
   provideNativeScriptRouter,
   runNativeScriptAngularApp,
 } from '@nativescript/angular';
-import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideZonelessChangeDetection } from '@angular/core';
 import { withInterceptorsFromDi } from '@angular/common/http';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-
-/**
- * Disable zone by setting this to true
- * Then also adjust polyfills.ts (see note there)
- */
-const EXPERIMENTAL_ZONELESS = false;
 
 runNativeScriptAngularApp({
   appModuleBootstrap: () => {
@@ -22,9 +15,7 @@ runNativeScriptAngularApp({
       providers: [
         provideNativeScriptHttpClient(withInterceptorsFromDi()),
         provideNativeScriptRouter(routes),
-        EXPERIMENTAL_ZONELESS
-          ? provideExperimentalZonelessChangeDetection()
-          : provideNativeScriptNgZone(),
+        provideZonelessChangeDetection(),
       ],
     });
   },
--- packages/template-hello-world-ng/src/polyfills.ts
+++ packages/template-hello-world-ng/src/polyfills.ts
@@ -6,20 +6,3 @@
 import '@nativescript/core/globals';
 // Install @nativescript/angular specific polyfills
 import '@nativescript/angular/polyfills';
-
-/**
- * Disable zone completely by removing the following 3 imports
- * alongside also adjusting main.ts to boot zoneless
- */
-
-/**
- * Zone.js and patches
- */
-// Add pre-zone.js patches needed for the NativeScript platform
-import '@nativescript/zone-js/dist/pre-zone-polyfills';
-
-// Zone JS is required by default for Angular itself
-import 'zone.js';
-
-// Add NativeScript specific Zone JS patches
-import '@nativescript/zone-js';
