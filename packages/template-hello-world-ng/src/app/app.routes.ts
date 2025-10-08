b5c6005db1b48b011cdb46313e707a5a20b3b3fe
Nathan Walker <walkerrunpdx@gmail.com>
2025-10-08 11:16:01 -0700

    fix: ng template updates

--- packages/template-hello-world-ng/src/app/app.routes.ts
+++ packages/template-hello-world-ng/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { PeopleComponent } from './people/person.component';
+import { PersonComponent } from './people/person.component';
 import { PersonDetailComponent } from './people/person-detail.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/items', pathMatch: 'full' },
-  { path: 'items', component: PeopleComponent },
+  { path: 'items', component: PersonComponent },
   { path: 'item/:id', component: PersonDetailComponent },
 ];
--- packages/template-hello-world-ng/src/app/people/person.component.ts
+++ packages/template-hello-world-ng/src/app/people/person.component.ts
@@ -1,26 +1,14 @@
 import { ChangeDetectionStrategy, Component, NO_ERRORS_SCHEMA, inject } from '@angular/core'
 import { NativeScriptCommonModule, NativeScriptRouterModule } from '@nativescript/angular'
-import { Page } from '@nativescript/core'
 import { PersonService } from './person.service'
 
 @Component({
-  selector: 'ns-people',
-  templateUrl: './people.component.html',
+  selector: 'ns-person',
+  templateUrl: './person.component.html',
   imports: [NativeScriptCommonModule, NativeScriptRouterModule],
   schemas: [NO_ERRORS_SCHEMA],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PeopleComponent {
+export class PersonComponent {
   personService = inject(PersonService);
-  page = inject(Page);
-
-  constructor() {
-    // Setup large titles on iOS
-    this.page.on('loaded', (args) => {
-      if (__IOS__) {
-        const navigationController: UINavigationController = this.page.frame.ios.controller;
-        navigationController.navigationBar.prefersLargeTitles = true;
-      }
-    })
-  }
 }
