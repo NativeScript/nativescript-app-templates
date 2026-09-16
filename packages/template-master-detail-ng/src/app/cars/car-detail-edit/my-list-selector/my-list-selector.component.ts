0984ebf9bf965b39ca6b30082c773ab4ee6b9420
Igor Randjelovic <rigor789@gmail.com>
2026-09-16 10:37:35 +0200

    fix(angular): satisfy strict template checks in the master-detail template
    Angular 22 type-checks templates: the door selector binds a number and
    a number array to inputs typed as strings, and the list tap event from
    a custom element is typed as Event.

--- packages/template-master-detail-ng/src/app/cars/car-detail-edit/my-list-selector/my-list-selector.component.ts
+++ packages/template-master-detail-ng/src/app/cars/car-detail-edit/my-list-selector/my-list-selector.component.ts
@@ -16,9 +16,9 @@ const capitalizeFirstLetter = (s) => s.charAt(0).toUpperCase() + s.slice(1)
 })
 export class MyListSelectorComponent implements OnInit {
   @Input() tag: string
-  @Input() items: Array<string>
-  @Input() selectedValue: string
-  @Output() selectedValueChange = new EventEmitter<string>()
+  @Input() items: Array<string | number>
+  @Input() selectedValue: string | number
+  @Output() selectedValueChange = new EventEmitter<string | number>()
 
   private _carEditModel: Car
 
@@ -56,7 +56,7 @@ export class MyListSelectorComponent implements OnInit {
 
     this._modalService
       .showModal(MyListSelectorModalViewComponent, options)
-      .then((selectedValue: string) => {
+      .then((selectedValue: string | number) => {
         if (selectedValue) {
           this.selectedValue = selectedValue
           this.selectedValueChange.emit(this.selectedValue)
